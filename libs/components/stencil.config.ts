c3102aae25967eb7c8e68b9b421b3e36d88746b9
Dabiel González Ramos <dabiel.ramos@endava.com>
2022-04-12 16:40:03 +0300

    chore(tailwind css): add and configure Tailwind CSS w/ StencilJs

--- libs/components/stencil.config.ts
+++ libs/components/stencil.config.ts
@@ -1,11 +1,25 @@
+import tailwindcss from 'tailwindcss';
+import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
 import { Config } from '@stencil/core';
-
 import { sass } from '@stencil/sass';
+
+import tailwindConf from './tailwind.config.js';
 
 export const config: Config = {
   namespace: 'components',
   taskQueue: 'async',
-  plugins: [sass()],
+  plugins: [
+    sass(),
+    tailwind({
+      stripComments: true,
+      tailwindCssPath: `${__dirname}/src/global/styles/tailwind.pcss`,
+      tailwindConf: tailwindConf,
+      postcss: {
+        plugins: [tailwindcss()],
+      },
+    }),
+    tailwindHMR(),
+  ],
   outputTargets: [
     {
       type: 'dist',
---
+++ libs/components/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
