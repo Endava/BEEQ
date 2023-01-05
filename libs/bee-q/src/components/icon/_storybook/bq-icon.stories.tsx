403f8b3aef17db339feb78a8a2c18e414f6b0bf1
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-01-05 15:43:18 +0200

    docs(icon): add a link to `Phosphor icons` on Storybook and show fewer SVGs (#64)

--- libs/bee-q/src/components/icon/_storybook/bq-icon.stories.tsx
+++ libs/bee-q/src/components/icon/_storybook/bq-icon.stories.tsx
@@ -3,6 +3,7 @@ import { repeat } from 'lit-html/directives/repeat.js';
 import mdx from './bq-icon.mdx';
 import { ICONS_SET } from '../helper/icons-set';
 import { ICON_WEIGHT } from '../bq-icon.types';
+import { getRandomFromArray } from '../../../shared/utils';
 
 export default {
   title: 'Components/Icon',
@@ -33,62 +34,54 @@ Default.args = {
   name: 'bell-ringing',
 };
 
-export const ExploreIcons = (args) => {
-  return html`
-    <style>
-      .icon-grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(75px, 1fr));
-        row-gap: 1.5rem;
-        -webkit-column-gap: 1rem;
-        column-gap: 1rem;
-        margin: 0 auto;
-      }
-      .icon-wrapper {
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-        text-align: center;
-        outline: 0;
-      }
-      .icon-wrapper:hover .icon-container {
-        box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.12);
-      }
-      .icon-container {
-        display: flex;
-        justify-content: center;
-        border: 1px solid var(--bq-ui--secondary);
-        border-radius: 4px;
-        margin-bottom: 8px;
-        padding: 16px 0;
-        transition-timing-function: ease-in-out;
-        transition-property: box-shadow;
-        transition-duration: 0.25s;
-        width: 100%;
-      }
-      .icon-span {
-        font-family: var(--bq-font-family--inter);
-        font-size: var(--bq-font-size--xs);
-        line-height: var(--bq-font-line-height--regular);
-        color: var(--bq-text--primary);
-      }
-    </style>
-    <div class="icon-grid">
-      ${repeat(
-        args.icons,
-        (icon) => icon,
-        (icon) => html`
-          <div class="icon-wrapper" role="button" tabindex="0">
-            <div class="icon-container">
-              <bq-icon color=${args.color} .name=${icon} size=${args.size} weight=${args.weight}></bq-icon>
-            </div>
-            <span class="icon-span">${icon}</span>
+export const ExploreIcons = (args) => html`
+  <style>
+    bq-button::part(button) {
+      text-decoration: none;
+    }
+  </style>
+  <div class="mb-8 text-text-primary">
+    <h1 class="text-xl font-bold">We didn't reinvent the wheel</h1>
+    <p class="mt-2">
+      Bee-Q icons are based on
+      <a
+        class="text-text-accent"
+        href="https://phosphoricons.com/"
+        target="_blank"
+        title="Phosphor icons"
+        rel="noreferrer noopener"
+      >
+        Phosphor icons library
+      </a>
+      , is a flexible icon family for interfaces, diagrams, presentations — whatever, really, is free and open-source,
+      licensed under MIT.
+    </p>
+    <span class="text-xs text-text-secondary">
+      (Below, you're seeing only a few examples of the icons that the library provides)
+    </span>
+  </div>
+  <bq-button class="mb-8" appearance="primary" href="https://phosphoricons.com/" target="_blank">
+    <bq-icon name="binoculars" weight="fill" slot="prefix"></bq-icon>
+    Explore all the icons available
+    <bq-icon class="ml-4" name="caret-right" weight="regular" slot="suffix"></bq-icon>
+  </bq-button>
+  <div class="icon-grid my-0 mx-auto grid grid-cols-[repeat(auto-fill,_minmax(75px,_1fr))] gap-6 gap-x-4">
+    ${repeat(
+      getRandomFromArray(args.icons, 36),
+      (icon) => icon,
+      (icon) => html`
+        <div class="group flex flex-col items-stretch text-center outline-0" role="button" tabindex="0">
+          <div
+            class="mb-2 flex w-full justify-center rounded-m border border-solid border-ui-secondary py-4 px-0 transition-shadow group-hover:shadow-m"
+          >
+            <bq-icon color=${args.color} .name=${icon as string} size=${args.size} weight=${args.weight}></bq-icon>
           </div>
-        `,
-      )}
-    </div>
-  `;
-};
+          <span class="text-s leading-regular text-text-primary">${icon}</span>
+        </div>
+      `,
+    )}
+  </div>
+`;
 ExploreIcons.args = {
   icons: ICONS_SET,
 };
