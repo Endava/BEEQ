afab74d4c9fa2745129027798b0eca484c1e576b
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-01-08 15:55:11 +0200

    chore(semver): custom conventional commits to add to the changelog

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {},
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'release'],
+    ],
+  },
 };
