9c396ba24a73ab0e6642f3abe3044194ba41cc7f
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-01-25 09:58:32 +0200

    chore(icons): add custom Nx executor to create the icons set (#78)

---
+++ libs/bee-q-icons/README.md
@@ -0,0 +1,7 @@
+# bee-q-icons
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build bee-q-icons` to build the library.
---
+++ libs/bee-q-icons/src/executors/generate-icons/helpers/extract.ts
@@ -0,0 +1,34 @@
+import * as decompress from 'decompress';
+import { copy, remove } from 'fs-extra';
+import { join } from 'node:path';
+
+interface IExtractIcons {
+  assetsFolder: string;
+  downloadPath: string;
+  extractToPath: string;
+  fileName: string;
+  svgFolder: string;
+}
+
+export const extractIcons = async ({
+  assetsFolder,
+  downloadPath,
+  extractToPath,
+  fileName,
+  svgFolder,
+}: IExtractIcons) => {
+  return new Promise<void>(async (resolve, reject) => {
+    try {
+      await decompress(join(downloadPath, fileName), downloadPath);
+      // Remove all existing files under the `/svg/` folder (if there's any) to create a clean copy
+      await remove(extractToPath);
+      // Move the SVG assets to the icon component `/svg/` folder
+      await copy(join(downloadPath, svgFolder, assetsFolder), extractToPath, {
+        overwrite: true,
+      });
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
