c5d4ba4a57481c9884b34145a42ad88d2e7c379c
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-02-27 09:32:05 +0200

    refactor(badge): inherit font-family

--- packages/bee-q/src/components/badge/bq-badge.tsx
+++ packages/bee-q/src/components/badge/bq-badge.tsx
@@ -129,11 +129,7 @@ export class BqBadge {
           }}
           part="base"
         >
-          <span
-            ref={(element) => (this.spanElement = element)}
-            class="font-inter text-xs font-bold leading-large"
-            part="number"
-          >
+          <span ref={(element) => (this.spanElement = element)} class="text-xs font-bold leading-large" part="number">
             <slot onSlotchange={this.onSlotChange}></slot>
           </span>
         </div>
