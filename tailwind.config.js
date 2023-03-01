83326a2fe3e25804c163d4b72f80ba28197ace60
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-03-01 10:41:57 +0200

    feat: add custom base styles to reset certain CSS rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['packages/bee-q/**/*.{jsx,js,tsx,ts}', 'packages/bee-q/**/_storybook/*.tsx'],
+  content: ['packages/bee-q/**/*.{jsx,js,tsx,ts}'],
   theme: {
     borderRadius: {
       none: 'var(--bq-radius--none)',
