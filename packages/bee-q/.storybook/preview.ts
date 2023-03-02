4a32ecab420560b6ae626b658705341112cbb3a1
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-03-02 21:39:43 +0200

    fix(storybook): remove useless import

--- packages/bee-q/.storybook/preview.ts
+++ packages/bee-q/.storybook/preview.ts
@@ -1,8 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import './css/styles.css';
-import { html } from 'lit-html';
-import { classMap } from 'lit-html/directives/class-map.js';
 import { setCustomElements } from '@storybook/web-components';
 
 import { defineCustomElements } from '@bee-q/core/dist/loader';
