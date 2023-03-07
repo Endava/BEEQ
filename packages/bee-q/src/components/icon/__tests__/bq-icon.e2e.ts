ded4d855cb3ae5dfc98781360441eee635e0d6ff
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-03-07 09:46:55 +0200

    chore: update download path for icon SVG assets (#31)
    Icon SVGs used under the hood on the icon components come from the Phosphor icon library, which now has moved the SVG assets to a core package: https://github.com/phosphor-icons/core.
    
    Details: https://github.com/phosphor-icons/web/releases/tag/v2.0.0

--- packages/bee-q/src/components/icon/__tests__/bq-icon.e2e.ts
+++ packages/bee-q/src/components/icon/__tests__/bq-icon.e2e.ts
@@ -30,20 +30,20 @@ describe('bq-icon', () => {
 
   it('should display icon', async () => {
     const page = await newE2EPage();
-    await page.setContent('<bq-icon name="activity"></bq-icon>');
+    await page.setContent('<bq-icon name="pulse"></bq-icon>');
 
     await page.$eval('bq-icon', waitForSvgLoad);
 
     const element = await page.find('bq-icon >>> [part="svg"]');
     expect(element.innerHTML).toBeDefined();
     expect(element.innerHTML).toEqualHtml(`
-      <path d="M160,216h-.4a8.1,8.1,0,0,1-7.1-5.2L95.3,60.8l-32,70.5A8,8,0,0,1,56,136H24a8,8,0,0,1,0-16H50.8L88.7,36.7A8.2,8.2,0,0,1,96.3,32a8,8,0,0,1,7.2,5.2L161,188.1l31.8-63.7A8.2,8.2,0,0,1,200,120h32a8,8,0,0,1,0,16H204.9l-37.7,75.6A8.2,8.2,0,0,1,160,216Z"></path>
+      <path d="M240,128a8,8,0,0,1-8,8H204.94l-37.78,75.58A8,8,0,0,1,160,216h-.4a8,8,0,0,1-7.08-5.14L95.35,60.76,63.28,131.31A8,8,0,0,1,56,136H24a8,8,0,0,1,0-16H50.85L88.72,36.69a8,8,0,0,1,14.76.46l57.51,151,31.85-63.71A8,8,0,0,1,200,120h32A8,8,0,0,1,240,128Z"></path>
     `);
   });
 
   it('should handle `name` property change', async () => {
     const page = await newE2EPage();
-    await page.setContent('<bq-icon name="activity"></bq-icon>');
+    await page.setContent('<bq-icon name="pulse"></bq-icon>');
 
     await setProperties(page, 'bq-icon', { name: 'check' });
     await page.$eval('bq-icon', waitForSvgLoad);
@@ -52,13 +52,13 @@ describe('bq-icon', () => {
     const element = await page.find('bq-icon >>> [part="svg"]');
     expect(element.innerHTML).toBeDefined();
     expect(element.innerHTML).toEqualHtml(`
-      <path d="M104,192a8.5,8.5,0,0,1-5.7-2.3l-56-56a8.1,8.1,0,0,1,11.4-11.4L104,172.7,210.3,66.3a8.1,8.1,0,0,1,11.4,11.4l-112,112A8.5,8.5,0,0,1,104,192Z"></path>
+      <path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"></path>
     `);
   });
 
   it('should respect design style', async () => {
     const page = await newE2EPage();
-    await page.setContent('<bq-icon name="activity"></bq-icon>');
+    await page.setContent('<bq-icon name="pulse"></bq-icon>');
 
     const style = await computedStyle(page, 'bq-icon >>> [part="base"]', ['height']);
 
