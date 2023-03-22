56371ede6215ff8c560ce3c1ef5c7a55d9e2cd44
Marina K <127310889+marinakostic@users.noreply.github.com>
2023-03-22 10:59:56 +0100

    feat(toast): add component storybook

---
+++ packages/bee-q/src/components/toast/_storybook/bq-toast.stories.tsx
@@ -0,0 +1,36 @@
+import { html } from 'lit-html';
+import { TOAST_TYPE } from '../bq-toast.types';
+import mdx from './bq-toast.mdx';
+
+export default {
+  title: 'Components/Toast',
+  component: 'bq-toast',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+  argTypes: {
+    type: { control: 'select', options: [...TOAST_TYPE] },
+    text: { control: 'text' },
+    textColor: { control: 'text' },
+    icon: { control: 'text' },
+  },
+  args: {
+    type: 'default',
+    text: 'This is toast message!',
+    textColor: 'data--blue',
+    icon: 'default',
+  },
+};
+
+const Template = (args) => {
+  return html`<bq-toast
+    text=${args.text}
+    type=${args.type}
+    icon=${args.icon}
+    text-color=${args.textColor}
+  ></bq-toast> `;
+};
+
+export const Default = (args) => Template(args);
