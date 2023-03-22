cc2630021421037c515da8d186fece20511f461e
urosendv <124157438+urosendv@users.noreply.github.com>
2023-03-22 12:37:34 +0100

    test(notification): refactor after slot changes

--- packages/bee-q/src/components/notification/__tests__/bq-notification.e2e.ts
+++ packages/bee-q/src/components/notification/__tests__/bq-notification.e2e.ts
@@ -21,39 +21,28 @@ describe('bq-notification', () => {
 
   it('should render basic notification', async () => {
     const page = await newE2EPage();
-    await page.setContent('<bq-notification description="You have a new Chat message">New Message</bq-notification>');
+    await page.setContent(
+      '<bq-notification>New Message<span slot="description">You have a new chat message</span></bq-notification>',
+    );
 
-    const description = await page.find('bq-notification >>> [id="description"]');
+    const description = await page.find('bq-notification >>> slot[name="description"]');
 
-    expect(description).toEqualText('You have a new Chat message');
+    expect(description).not.toBeNull();
   });
 
   it('should show notification with icon', async () => {
     const page = await newE2EPage();
-    await page.setContent(
-      '<bq-notification type="info" description="You have a new Chat message" show-icon="true">New Message</bq-notification>',
-    );
+    await page.setContent('<bq-notification type="info" show-icon="true">New Message</bq-notification>');
 
     const iconHolder = await page.find('bq-notification >>> [id="notification-icon-holder"]');
 
     expect(iconHolder).not.toBeNull();
   });
 
-  it('should show url after notification description', async () => {
-    const page = await newE2EPage();
-    await page.setContent(
-      '<bq-notification description="You have a new Chat message" href="https://example.com">New Message</bq-notification>',
-    );
-
-    const urlContent = await page.find('bq-notification >>> [id="notification-url"]');
-
-    expect(urlContent).not.toBeNull();
-  });
-
   it('should show notification button(s)', async () => {
     const page = await newE2EPage();
     await page.setContent(
-      '<bq-notification description="You have a new Chat message">New Message<bq-button appearance="primary" href="" size="small" target="" type="button" variant="standard">Button</bq-button><bq-button name="second-button" appearance="secondary" href="" size="small" target="" type="button" variant="standard">Button</bq-button></bq-notification>',
+      '<bq-notification>New Message<bq-button appearance="primary" href="" size="small" target="" type="button" variant="standard">Button</bq-button><bq-button name="second-button" appearance="secondary" href="" size="small" target="" type="button" variant="standard">Button</bq-button></bq-notification>',
     );
 
     const button = await page.find('bq-button');
@@ -64,7 +53,7 @@ describe('bq-notification', () => {
   it('should show notification with avatar', async () => {
     const page = await newE2EPage();
     await page.setContent(
-      '<bq-notification description="You have a new Chat message" href="https://example.com">New Message <bq-avatar slot="avatar" alt-text="" image="" label="Avatar component label" initials="JS" shape="circle" size="small"></bq-avatar></bq-notification>',
+      '<bq-notification>New Message <bq-avatar slot="avatar" alt-text="" image="" label="Avatar component label" initials="JS" shape="circle" size="small"></bq-avatar></bq-notification>',
     );
 
     const avatarHolder = await page.find('bq-notification >>> [id="notification-avatar-holder"]');
@@ -76,9 +65,7 @@ describe('bq-notification', () => {
 
   it('should hide notification on click of close icon', async () => {
     const page = await newE2EPage();
-    await page.setContent(
-      '<bq-notification description="You have a new Chat message" show-close="true">New Message</bq-notification>',
-    );
+    await page.setContent('<bq-notification show-close="true">New Message</bq-notification>');
 
     const notificationHolder = await page.find('bq-notification');
     const closeIcon = await page.find('bq-notification >>> [id="close-notification"]');
