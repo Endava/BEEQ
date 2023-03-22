807ab2bdb70572a99ca7410421c3fc00acacf984
Marina K <127310889+marinakostic@users.noreply.github.com>
2023-03-22 11:06:05 +0100

    test(toast): initial commit
    (cherry picked from commit 62e2609a1bb4714f602024b70487a31f1ed90fda)

---
+++ packages/bee-q/src/components/toast/__tests__/bq-toast.e2e.ts
@@ -0,0 +1,30 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('bq-toast', () => {
+  it('should render', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<bq-toast></bq-toast>');
+
+    const element = await page.find('bq-toast');
+
+    expect(element).toHaveClass('hydrated');
+  });
+
+  it('should have shadow root', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<bq-toast></bq-toast>');
+
+    const element = await page.find('bq-toast');
+
+    expect(element.shadowRoot).not.toBeNull();
+  });
+
+  it('should display text', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<bq-toast></bq-toast>');
+
+    const element = await page.find('bq-toast >>> p');
+
+    expect(element).toEqualText('My name is Stencil');
+  });
+});
