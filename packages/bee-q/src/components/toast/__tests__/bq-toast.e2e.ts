a9e61fba3464944d92acefbb5d4302939a159a6d
Marina K <127310889+marinakostic@users.noreply.github.com>
2023-03-28 13:17:51 +0200

    test(toast): added component e2es
    (cherry picked from commit dcb087bed5748bba16e1063d9420d9b56d7576a3)

--- packages/bee-q/src/components/toast/__tests__/bq-toast.e2e.ts
+++ packages/bee-q/src/components/toast/__tests__/bq-toast.e2e.ts
@@ -19,12 +19,41 @@ describe('bq-toast', () => {
     expect(element.shadowRoot).not.toBeNull();
   });
 
-  it('should display text', async () => {
+  it('should be visible on button click', async () => {
     const page = await newE2EPage();
-    await page.setContent('<bq-toast></bq-toast>');
+    await page.setContent(`
+      <bq-button appearance="primary" size="small" target="" type="button" variant="standard">
+        Show Toast!
+      </bq-button>
+      <bq-toast>
+        <bq-icon slot="icon" name="info" color="ui--brand" size="24" weight="bold"></bq-icon>
+        <span slot="text">This is some toast text message! </span></bq-toast
+      >
+    `);
 
-    const element = await page.find('bq-toast >>> p');
+    const element = await page.find('bq-toast >>> [part="panel"]');
+    expect(element).not.toBeDefined;
 
-    expect(element).toEqualText('My name is Stencil');
+    const button = await page.find('bq-button');
+    await button.click();
+    expect(element).toBeDefined;
+  });
+
+  it('should render basic toast', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<bq-toast>New Toast<span slot="text">This is some toast text message!</span></bq-toast>');
+
+    const text = await page.find('bq-toast >>> slot[name="text"]');
+
+    expect(text).not.toBeNull();
+  });
+
+  it('should show toast with icon', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<bq-toast type="info" show-icon="true">New Toast Message!</bq-toast>');
+
+    const iconHolder = await page.find('bq-toast >>> [part="icon"]');
+
+    expect(iconHolder).not.toBeNull();
   });
 });
