333fd8be6f5ace15014aaeab9f8eb85a35951f62
Hamude Homsi <Hamude.Homsi@endava.com>
2023-03-30 16:39:22 +0300

    feat(menu): add storybook doc

--- packages/bee-q/src/components/menu/_storybook/bq-menu.stories.tsx
+++ packages/bee-q/src/components/menu/_storybook/bq-menu.stories.tsx
@@ -1,4 +1,6 @@
 import { html } from 'lit-html';
+
+import { MENU_SIZE, MENU_THEME } from '../bq-menu.types';
 import mdx from './bq-menu.mdx';
 
 export default {
@@ -10,15 +12,73 @@ export default {
     },
   },
   argTypes: {
+    size: { control: 'select', options: [...MENU_SIZE] },
+    collapsible: { control: 'boolean' },
+    theme: { control: 'select', options: [...MENU_THEME] },
     text: { control: 'text', table: { disable: true } },
+    // Event handlers
+    bqBlur: { action: 'bqBlur' },
+    bqFocus: { action: 'bqFocus' },
+    bqClick: { action: 'bqClick' },
   },
   args: {
+    size: 'medium',
+    collapsible: true,
+    theme: 'light',
     text: 'text',
   },
 };
 
 const Template = (args) => {
-  return html`<bq-menu>${args.text}</bq-menu>`;
+  return html`<bq-menu
+    size=${args.size}
+    ?collapsible=${args.collapsible}
+    theme=${args.theme}
+    @bqBlur=${args.bqBlur}
+    @bqFocus=${args.bqFocus}
+    @bqClick=${args.bqClick}
+  >
+    <!-- header slot -->
+    <h1 slot="header">Bee-Q</h1>
+
+    <!-- content -->
+    <bq-menu-item>
+      <bq-icon size="18" name="user" slot="prefix"></bq-icon>
+      <span>Verified users</span>
+      <bq-icon size="18" name="check" color="#34eb52" slot="suffix"></bq-icon>
+    </bq-menu-item>
+
+    <bq-menu-item>
+      <bq-icon size="18" name="presentation-chart" slot="prefix"></bq-icon>
+      <span>Dashboard</span>
+    </bq-menu-item>
+
+    <bq-menu-item disabled="true">
+      <span>Do not enter</span>
+      <bq-icon size="18" name="prohibit" slot="suffix"></bq-icon>
+    </bq-menu-item>
+
+    <!-- footer slot -->
+    <div slot="footer">Collapsible</div>
+  </bq-menu>`;
 };
 
-export const Default = (args) => Template(args);
+export const Light = (args) => Template(args);
+Light.args = {
+  theme: 'light',
+};
+
+export const Dark = (args) => Template(args);
+Dark.args = {
+  theme: 'dark',
+};
+
+export const Small = (args) => Template(args);
+Small.args = {
+  size: 'small',
+};
+
+export const Medium = (args) => Template(args);
+Medium.args = {
+  size: 'medium',
+};
