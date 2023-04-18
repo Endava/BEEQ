3ab704bd5d0ccbd74018439b016c466a76f64c9a
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-04-18 19:07:13 +0300

    docs(Button): update Storybook documentation to CSF3

--- packages/bee-q/src/components/button/_storybook/bq-button.stories.tsx
+++ packages/bee-q/src/components/button/_storybook/bq-button.stories.tsx
@@ -1,9 +1,11 @@
-import { html } from 'lit-html';
+import { html, render } from 'lit-html';
 
 import { BUTTON_APPEARANCE, BUTTON_SIZE, BUTTON_TYPE, BUTTON_VARIANT } from '../bq-button.types';
 import mdx from './bq-button.mdx';
 
-export default {
+import type { Args, Meta, StoryObj } from '@storybook/web-components';
+
+const meta: Meta = {
   title: 'Components/Button',
   component: 'bq-button',
   parameters: {
@@ -39,7 +41,10 @@ export default {
   },
 };
 
-const Template = (args) => html`
+export default meta;
+type Story = StoryObj;
+
+const Template = (args: Args) => html`
   <bq-button
     appearance=${args.appearance}
     ?disabled=${args.disabled}
@@ -57,90 +62,106 @@ const Template = (args) => html`
   </bq-button>
 `;
 
-export const Primary = (args) => Template(args);
-Primary.args = {
-  buttonText: 'Primary button',
+export const Primary: Story = {
+  render: Template.bind({}),
+  args: {
+    buttonText: 'Primary button',
+  },
 };
 
-export const Secondary = (args) => Template(args);
-Secondary.args = {
-  appearance: 'secondary',
-  buttonText: 'Secondary button',
+export const Secondary: Story = {
+  render: Template.bind({}),
+  args: {
+    appearance: 'secondary',
+    buttonText: 'Secondary button',
+  },
 };
 
-export const Link = (args) => Template(args);
-Link.args = {
-  appearance: 'link',
-  href: 'https://www.example.com',
-  target: '_blank',
-  buttonText: 'Link button',
+export const Link: Story = {
+  render: Template.bind({}),
+  args: {
+    appearance: 'link',
+    href: 'https://www.example.com',
+    target: '_blank',
+    buttonText: 'Link button',
+  },
 };
 
-export const Text = (args) => Template(args);
-Text.args = {
-  appearance: 'text',
-  buttonText: 'Text button',
+export const Text: Story = {
+  render: Template.bind({}),
+  args: {
+    appearance: 'text',
+    buttonText: 'Text button',
+  },
 };
 
-export const Loading = (args) => Template(args);
-Loading.args = {
-  appearance: 'primary',
-  loading: true,
-  buttonText: 'Loading button',
+export const Loading: Story = {
+  render: Template.bind({}),
+  args: {
+    appearance: 'primary',
+    loading: true,
+    buttonText: 'Loading button',
+  },
 };
 
-export const IconLeft = (args) => html`
-  <bq-button
-    appearance=${args.appearance}
-    ?disabled=${args.disabled}
-    href=${args.href}
-    ?loading=${args.loading}
-    size=${args.size}
-    target=${args.target}
-    type=${args.type}
-    variant=${args.variant}
-    @bqBlur=${args.bqBlur}
-    @bqClick=${args.bqClick}
-    @bqFocus=${args.bqFocus}
-  >
-    <bq-icon name="arrow-circle-left" slot="prefix"></bq-icon>
-    Go back
-  </bq-button>
-`;
+export const IconLeft: Story = {
+  render: (args) => html`
+    <bq-button
+      appearance=${args.appearance}
+      ?disabled=${args.disabled}
+      href=${args.href}
+      ?loading=${args.loading}
+      size=${args.size}
+      target=${args.target}
+      type=${args.type}
+      variant=${args.variant}
+      @bqBlur=${args.bqBlur}
+      @bqClick=${args.bqClick}
+      @bqFocus=${args.bqFocus}
+    >
+      <bq-icon name="arrow-circle-left" slot="prefix"></bq-icon>
+      Go back
+    </bq-button>
+  `,
+};
 
-export const IconRight = (args) => html`
-  <bq-button
-    appearance=${args.appearance}
-    ?disabled=${args.disabled}
-    href=${args.href}
-    ?loading=${args.loading}
-    size=${args.size}
-    target=${args.target}
-    type=${args.type}
-    variant=${args.variant}
-    @bqBlur=${args.bqBlur}
-    @bqClick=${args.bqClick}
-    @bqFocus=${args.bqFocus}
-  >
-    Next step
-    <bq-icon name="arrow-circle-right" slot="suffix"></bq-icon>
-  </bq-button>
-`;
+export const IconRight: Story = {
+  render: (args) => html`
+    <bq-button
+      appearance=${args.appearance}
+      ?disabled=${args.disabled}
+      href=${args.href}
+      ?loading=${args.loading}
+      size=${args.size}
+      target=${args.target}
+      type=${args.type}
+      variant=${args.variant}
+      @bqBlur=${args.bqBlur}
+      @bqClick=${args.bqClick}
+      @bqFocus=${args.bqFocus}
+    >
+      Next step
+      <bq-icon name="arrow-circle-right" slot="suffix"></bq-icon>
+    </bq-button>
+  `,
+};
 
-export const OnlyIcon = (args) => html`
-  <bq-button
-    appearance=${args.appearance}
-    ?disabled=${args.disabled}
-    href=${args.href}
-    ?loading=${args.loading}
-    size=${args.size}
-    target=${args.target}
-    type=${args.type}
-    variant=${args.variant}
-    @bqBlur=${args.bqBlur}
-    @bqClick=${args.bqClick}
-    @bqFocus=${args.bqFocus}
-  >
-    <bq-icon name="bell-ringing"></bq-icon>
-  </bq-button>
-`;
+export const OnlyIcon: Story = {
+  render: (args) => html`
+    <bq-button
+      appearance=${args.appearance}
+      ?disabled=${args.disabled}
+      href=${args.href}
+      ?loading=${args.loading}
+      size=${args.size}
+      target=${args.target}
+      type=${args.type}
+      variant=${args.variant}
+      @bqBlur=${args.bqBlur}
+      @bqClick=${args.bqClick}
+      @bqFocus=${args.bqFocus}
+    >
+      <bq-icon name="bell-ringing"></bq-icon>
+    </bq-button>
+  `,
+};
