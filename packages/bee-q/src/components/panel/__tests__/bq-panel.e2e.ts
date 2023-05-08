0c2bb6d15dedc3d7f4445b693b13adcafc492bc5
Hamude Homsi <Hamude.Homsi@endava.com>
2023-05-08 10:54:29 +0300

    feat(panel): add e2e tests

--- packages/bee-q/src/components/panel/__tests__/bq-panel.e2e.ts
+++ packages/bee-q/src/components/panel/__tests__/bq-panel.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
+import { computedStyle } from '../../../shared/test-utils';
 
 describe('bq-panel', () => {
   it('should render', async () => {
@@ -19,12 +20,38 @@ describe('bq-panel', () => {
     expect(element.shadowRoot).not.toBeNull();
   });
 
-  it('should display text', async () => {
+  it('should be visible on click', async () => {
     const page = await newE2EPage();
-    await page.setContent('<bq-panel></bq-panel>');
+    await page.setContent(`
+      <bq-dropdown>
+        <bq-button slot="trigger">Open</bq-button>
 
-    const element = await page.find('bq-panel >>> p');
+        <bq-panel>Some content in panel</bq-panel>
+      </bq-dropdown>
+    `);
 
-    expect(element).toEqualText('My name is Stencil');
+    const element = await page.find('bq-panel');
+    expect(element).toHaveAttribute('aria-hidden');
+
+    const button = await page.find('bq-button');
+    await button.click();
+    expect(element).not.toHaveAttribute('aria-hidden');
+  });
+
+  it('should show in specified position', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-dropdown>
+        <bq-button slot="trigger">Open</bq-button>
+
+        <bq-panel placement="left">Some content in panel</bq-panel>
+      </bq-dropdown>
+    `);
+
+    const button = await page.find('bq-button');
+    await button.click();
+    const tooltipStyle = await computedStyle(page, 'bq-panel');
+    const sign = tooltipStyle.left.slice(-1);
+    expect(sign).not.toEqual('-');
   });
 });
