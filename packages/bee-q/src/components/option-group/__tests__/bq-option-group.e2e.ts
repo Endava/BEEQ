b6aadb09fe763c0004c7102e88fc798877ae7aa4
Hamude Homsi <Hamude.Homsi@endava.com>
2023-05-12 12:08:30 +0300

    feat(option-group): add e2e tests

--- packages/bee-q/src/components/option-group/__tests__/bq-option-group.e2e.ts
+++ packages/bee-q/src/components/option-group/__tests__/bq-option-group.e2e.ts
@@ -18,4 +18,73 @@ describe('bq-option-group', () => {
 
     expect(element.shadowRoot).not.toBeNull();
   });
+
+  it('should render prefix element', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-option-group>
+        <span slot="label-prefix">Prefix text</span>
+        <span slot="label">Food</span>
+        <bq-option value="pizza">
+          <bq-icon name="pizza" size="16" slot="prefix"></bq-icon>
+          <span>Pizza</span>
+        </bq-option>
+      </bq-option-group>
+    `);
+
+    const prefixText = await page.$eval('bq-option-group', (element) => {
+      const slotElement = element.shadowRoot.querySelector('slot[name="label-prefix"]');
+      const assignedElements = (slotElement as HTMLSlotElement).assignedElements({ flatten: true })[0];
+
+      return assignedElements.textContent;
+    });
+
+    expect(prefixText).toBe('Prefix text');
+  });
+
+  it('should render suffix element', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-option-group>
+      <span slot="label">Food</span>
+      <span slot="label-suffix">Suffix text</span>
+        <bq-option value="pizza">
+          <bq-icon name="pizza" size="16" slot="prefix"></bq-icon>
+          <span>Pizza</span>
+        </bq-option>
+      </bq-option-group>
+    `);
+
+    const suffixText = await page.$eval('bq-option-group', (element) => {
+      const slotElement = element.shadowRoot.querySelector('slot[name="label-suffix"]');
+      const assignedElements = (slotElement as HTMLSlotElement).assignedElements({ flatten: true })[0];
+
+      return assignedElements.textContent;
+    });
+
+    expect(suffixText).toEqualText('Suffix text');
+  });
+
+  it('should render label text', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-option-group>
+      <bq-icon name="fork-knife" size="16" slot="label-prefix"></bq-icon>
+      <span slot="label">Food</span>
+        <bq-option value="pizza">
+          <bq-icon name="pizza" size="16" slot="prefix"></bq-icon>
+          <span>Pizza</span>
+        </bq-option>
+      </bq-option-group>
+    `);
+
+    const suffixText = await page.$eval('bq-option-group', (element) => {
+      const slotElement = element.shadowRoot.querySelector('slot[name="label"]');
+      const assignedElements = (slotElement as HTMLSlotElement).assignedElements({ flatten: true })[0];
+
+      return assignedElements.textContent;
+    });
+
+    expect(suffixText).toEqualText('Food');
+  });
 });
