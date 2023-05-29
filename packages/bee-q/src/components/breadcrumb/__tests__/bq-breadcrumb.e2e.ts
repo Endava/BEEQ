b3922204a7f32f84c84c08ad2882d89f1f0fabc8
Hamude Homsi <Hamude.Homsi@endava.com>
2023-05-29 20:23:21 +0300

    feat(breadcrumb): add e2e tests

--- packages/bee-q/src/components/breadcrumb/__tests__/bq-breadcrumb.e2e.ts
+++ packages/bee-q/src/components/breadcrumb/__tests__/bq-breadcrumb.e2e.ts
@@ -18,4 +18,64 @@ describe('bq-breadcrumb', () => {
 
     expect(element.shadowRoot).not.toBeNull();
   });
+
+  it('should trigger bqBreadcrumbClick', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-breadcrumb>
+        <bq-breadcrumb-item>Home</bq-breadcrumb-item>
+      </bq-breadcrumb>
+    `);
+
+    const bqBlur = await page.spyOnEvent('bqBreadcrumbBlur');
+    const bqFocus = await page.spyOnEvent('bqBreadcrumbFocus');
+    const bqClick = await page.spyOnEvent('bqBreadcrumbClick');
+
+    const element = await page.find('bq-breadcrumb-item');
+
+    await element.click();
+
+    expect(bqFocus).toHaveReceivedEventTimes(1);
+    expect(bqClick).toHaveReceivedEventTimes(1);
+    expect(bqBlur).toHaveReceivedEventTimes(0);
+  });
+
+  it('should be keyboard accessible', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-breadcrumb>
+        <bq-breadcrumb-item>Home</bq-breadcrumb-item>
+      </bq-breadcrumb>
+    `);
+
+    const bqBlur = await page.spyOnEvent('bqBreadcrumbBlur');
+    const bqFocus = await page.spyOnEvent('bqBreadcrumbFocus');
+    const bqClick = await page.spyOnEvent('bqBreadcrumbClick');
+
+    await page.keyboard.press('Tab');
+
+    expect(bqFocus).toHaveReceivedEventTimes(1);
+    expect(bqClick).toHaveReceivedEventTimes(0);
+    expect(bqBlur).toHaveReceivedEventTimes(0);
+  });
+
+  it('should trigger bqBreadcrumbClick on Enter', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <bq-breadcrumb>
+        <bq-breadcrumb-item>Home</bq-breadcrumb-item>
+      </bq-breadcrumb>
+    `);
+
+    const bqBlur = await page.spyOnEvent('bqBreadcrumbBlur');
+    const bqFocus = await page.spyOnEvent('bqBreadcrumbFocus');
+    const bqClick = await page.spyOnEvent('bqBreadcrumbClick');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+
+    expect(bqFocus).toHaveReceivedEventTimes(1);
+    expect(bqClick).toHaveReceivedEventTimes(1);
+    expect(bqBlur).toHaveReceivedEventTimes(0);
+  });
 });
