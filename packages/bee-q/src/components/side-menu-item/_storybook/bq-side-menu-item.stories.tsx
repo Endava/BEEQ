4f4a0d4763d3f08246fbdb00e3ec60fed795618c
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-06-20 20:57:28 +0300

    docs(side menu item): update storybook example

--- packages/bee-q/src/components/side-menu-item/_storybook/bq-side-menu-item.stories.tsx
+++ packages/bee-q/src/components/side-menu-item/_storybook/bq-side-menu-item.stories.tsx
@@ -13,12 +13,18 @@ const meta: Meta = {
   },
   argTypes: {
     active: { control: 'boolean' },
+    disabled: { control: 'boolean' },
     collapse: { control: 'boolean' },
     // Not part of the component
     text: { control: 'text', table: { disable: true } },
+    // Event handlers
+    bqBlur: { action: 'bqBlur' },
+    bqFocus: { action: 'bqFocus' },
+    bqClick: { action: 'bqClick' },
   },
   args: {
     active: false,
+    disabled: false,
     collapse: false,
     // Not part of the component
     text: 'Menu item',
@@ -29,7 +35,14 @@ export default meta;
 type Story = StoryObj;
 
 const Template = (args: Args) => html`
-  <bq-side-menu-item ?active=${args.active} ?collapse=${args.collapse}>
+  <bq-side-menu-item
+    ?active=${args.active}
+    ?disabled=${args.disabled}
+    ?collapse=${args.collapse}
+    @bqBlur=${args.bqBlur}
+    @bqClick=${args.bqClick}
+    @bqFocus=${args.bqFocus}
+  >
     <bq-icon name="star-four" slot="prefix"></bq-icon>
     ${args.text}
     <bq-badge class="ml-auto" slot="suffix"> 5 </bq-badge>
