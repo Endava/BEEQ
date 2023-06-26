3dcc0d11ed17dbecb182d30158b70d6be099afb0
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-06-26 21:24:06 +0300

    docs(dialog): update storybook examples

--- packages/bee-q/src/components/dialog/_storybook/bq-dialog.stories.tsx
+++ packages/bee-q/src/components/dialog/_storybook/bq-dialog.stories.tsx
@@ -1,5 +1,5 @@
 import type { Args, Meta, StoryObj } from '@storybook/web-components';
-import { html } from 'lit-html';
+import { html, nothing } from 'lit-html';
 
 import mdx from './bq-dialog.mdx';
 
@@ -14,17 +14,26 @@ const meta: Meta = {
     },
   },
   argTypes: {
-    text: { control: 'text', table: { disable: true } },
-    size: { control: 'select', options: [...DIALOG_SIZE] },
+    'disable-close-click-outside': { control: 'boolean' },
+    'disable-close-esc-keydown': { control: 'boolean' },
     'footer-apperance': { control: 'select', options: [...DIALOG_FOOTER_APPEARANCE] },
     'hide-close-button': { control: 'boolean' },
-    'disable-outside-click-close': { control: 'boolean' },
-    'disable-esc-key-down-close': { control: 'boolean' },
+    open: { control: 'boolean' },
+    size: { control: 'select', options: [...DIALOG_SIZE] },
+    // Not part of the public API
+    noContent: { control: 'boolean', table: { disable: true } },
+    noFooter: { control: 'boolean', table: { disable: true } },
   },
   args: {
-    text: 'text',
-    size: 'medium',
+    'disable-close-click-outside': false,
+    'disable-close-esc-keydown': false,
+    'hide-close-button': false,
     'footer-apperance': 'standard',
+    open: false,
+    size: 'medium',
+    // Not part of the public API
+    noContent: false,
+    noFooter: false,
   },
 };
 
@@ -35,31 +44,39 @@ type Story = StoryObj;
 const Template = (args: Args) => {
   const handleOpenDialog = async () => {
     const dialogElem = document.querySelector('bq-dialog');
-    await dialogElem.open();
+    await dialogElem.show();
   };
 
   return html`
     <bq-button @bqClick=${handleOpenDialog}>Open Dialog</bq-button>
     <bq-dialog
-      size=${args.size}
+      ?disable-close-esc-keydown=${args['disable-close-esc-keydown']}
+      ?disable-close-click-outside=${args['disable-close-click-outside']}
       footer-apperance=${args['footer-apperance']}
       ?hide-close-button=${args['hide-close-button']}
-      ?disable-outside-click-close=${args['disable-outside-click-close']}
-      ?disable-esc-key-down-close=${args['disable-esc-key-down-close']}
+      ?open=${args.open}
+      size=${args.size}
     >
-      <bq-icon name="info" color="text--accent" role="img" title="Info" part="icon-on" slot="icon"></bq-icon>
+      <bq-icon name="info" size="30" color="text--accent" role="img" title="Info" slot="icon"></bq-icon>
       <h3 slot="title">Title</h3>
-      <p slot="description">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book.
-      </p>
-      <bq-button appearance="primary" size="medium" type="button" variant="ghost" class="hydrated" slot="footer">
-        Ghost button
-      </bq-button>
-      <bq-button appearance="primary" size="medium" type="button" variant="standard" slot="footer">
-        Standard button
-      </bq-button>
+      ${!args.noContent
+        ? html`
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
+            </p>
+          `
+        : nothing}
+      ${!args.noFooter
+        ? html`
+            <div class="flex gap-xs" slot="footer">
+              <bq-button appearance="link"> Button </bq-button>
+              <bq-button variant="ghost"> Button </bq-button>
+              <bq-button variant="standard" slot="footer"> Button </bq-button>
+            </div>
+          `
+        : nothing}
     </bq-dialog>
   `;
 };
@@ -68,50 +85,41 @@ export const Default: Story = {
   render: Template,
 };
 
+export const NoFooter: Story = {
+  render: Template,
+  args: {
+    noFooter: true,
+  },
+};
+
 const ConfirmTemplate = (args: Args) => {
   const handleOpenDialog = async () => {
     const dialogElem = document.querySelector('bq-dialog');
-    await dialogElem.open();
+    await dialogElem.show();
   };
 
   const handleCloseDialog = async () => {
     const dialogElem = document.querySelector('bq-dialog');
-    await dialogElem.close();
+    await dialogElem.hide();
   };
 
   return html`
     <bq-button @bqClick=${handleOpenDialog}>Open Confirm Dialog</bq-button>
     <bq-dialog
-      size=${args.size}
+      ?disable-close-esc-keydown=${args['disable-close-esc-keydown']}
+      ?disable-close-click-outside=${args['disable-close-click-outside']}
       footer-apperance=${args['footer-apperance']}
       ?hide-close-button=${args['hide-close-button']}
-      ?disable-outside-click-close=${args['disable-outside-click-close']}
-      ?disable-esc-key-down-close=${args['disable-esc-key-down-close']}
+      ?open=${args.open}
+      size=${args.size}
     >
-      <h3 slot="title">Lorem Ipsum ?</h3>
-      <p slot="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-      <bq-button
-        appearance="primary"
-        size="medium"
-        type="button"
-        variant="ghost"
-        class="hydrated"
-        @bqClick=${handleCloseDialog}
-        slot="footer"
-      >
-        Disagree
-      </bq-button>
-      <bq-button
-        appearance="primary"
-        size="medium"
-        type="button"
-        variant="standard"
-        class="hydrated"
-        @bqClick=${handleCloseDialog}
-        slot="footer"
-      >
-        Agree
-      </bq-button>
+      <h3 slot="title">Please confirm</h3>
+      <p>Are your sure you want to leave this page?</p>
+      <span> All unsaved work will be lost. </span>
+      <div class="flex gap-xs" slot="footer">
+        <bq-button variant="ghost" @bqClick=${handleCloseDialog}> Yes, I confirm </bq-button>
+        <bq-button variant="standard" slot="footer"> No, I want to stay </bq-button>
+      </div>
     </bq-dialog>
   `;
 };
@@ -119,8 +127,8 @@ const ConfirmTemplate = (args: Args) => {
 export const Confirm: Story = {
   render: ConfirmTemplate,
   args: {
+    'disable-close-click-outside': true,
+    'disable-close-esc-keydown': true,
     'hide-close-button': true,
-    'disable-outside-click-close': true,
-    'disable-esc-key-down-close': true,
   },
 };
