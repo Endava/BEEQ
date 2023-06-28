7f64ee3100f13594bc6ec7088b4ee10b46da1a0d
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-06-28 11:24:30 +0300

    fix(angular wrapper): this.appInits[i] is not a function (#304)
    Solution provided by @mavgustin

--- packages/bee-q-angular/src/bee-q.module.ts
+++ packages/bee-q-angular/src/bee-q.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { defineCustomElements } from '@bee-q/core/dist/loader';
 
 import { DIRECTIVES } from './directives';
@@ -8,18 +8,10 @@ import { DIRECTIVES } from './directives';
   imports: [CommonModule],
   declarations: [...DIRECTIVES],
   exports: [...DIRECTIVES],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: () => {
-        return defineCustomElements();
-      },
-      multi: true,
-    },
-  ],
 })
 export class BeeQModule {
   static forRoot(): ModuleWithProviders<BeeQModule> {
+    defineCustomElements();
     return {
       ngModule: BeeQModule,
     };
