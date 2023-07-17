3888675fb7ee6de71c0d02f57174f8c7aeba5d32
Dabiel González Ramos <dabiel.ramos@endava.com>
2023-07-17 13:54:35 +0300

    fix(tooltip): set `display: flex` on the trigger container

--- packages/bee-q/src/components/tooltip/bq-tooltip.tsx
+++ packages/bee-q/src/components/tooltip/bq-tooltip.tsx
@@ -173,7 +173,7 @@ export class BqTooltip {
       <div class="bq-tooltip relative" part="base">
         {/* TRIGGER */}
         <div
-          class="bq-tooltip--trigger"
+          class="bq-tooltip--trigger flex"
           onMouseOver={this.handleTriggerMouseOver}
           onMouseLeave={this.handleTriggerMouseLeave}
           onClick={this.handleTriggerOnClick}
