1c6a49596cadb0f775b094828d3e462ab14d0a29
Bogdan Bosca <bogdan-dan.bosca@endava.com>
2023-07-29 03:19:01 +0300

    fix(tab): update active tab when value is changed externally

--- packages/bee-q/src/components/tab-group/__tests__/bq-tab-group.e2e.ts
+++ packages/bee-q/src/components/tab-group/__tests__/bq-tab-group.e2e.ts
@@ -2,13 +2,14 @@ import { newE2EPage } from '@stencil/core/testing';
 
 describe('bq-tab-group', () => {
   it('should render', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+    const page = await newE2EPage({
+      html: `
     <bq-tab-group value="1">
       <bq-tab tab-id="1">Tab 1</bq-tab>
       <bq-tab tab-id="2">Tab 2</bq-tab>
     </bq-tab-group>
-    `);
+    `,
+    });
 
     const element = await page.find('bq-tab-group');
 
@@ -16,13 +17,14 @@ describe('bq-tab-group', () => {
   });
 
   it('should have shadow root', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+    const page = await newE2EPage({
+      html: `
     <bq-tab-group value="1">
       <bq-tab tab-id="1">Tab 1</bq-tab>
       <bq-tab tab-id="2">Tab 2</bq-tab>
     </bq-tab-group>
-    `);
+    `,
+    });
 
     const element = await page.find('bq-tab-group');
 
@@ -30,13 +32,14 @@ describe('bq-tab-group', () => {
   });
 
   it('should change size of all tabs', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+    const page = await newE2EPage({
+      html: `
     <bq-tab-group value="1" size="medium">
       <bq-tab tab-id="1">Tab 1</bq-tab>
       <bq-tab tab-id="2">Tab 2</bq-tab>
     </bq-tab-group>
-    `);
+    `,
+    });
 
     const element = await page.find('bq-tab');
 
@@ -44,13 +47,14 @@ describe('bq-tab-group', () => {
   });
 
   it('should emit bqChange on tab click', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+    const page = await newE2EPage({
+      html: `
     <bq-tab-group value="1">
       <bq-tab tab-id="1">Tab 1</bq-tab>
       <bq-tab tab-id="2">Tab 2</bq-tab>
     </bq-tab-group>
-    `);
+    `,
+    });
 
     const bqFocus = await page.spyOnEvent('bqFocus');
     const bqChange = await page.spyOnEvent('bqChange');
@@ -65,15 +69,16 @@ describe('bq-tab-group', () => {
   });
 
   it('should emit bqChange on keyboard navigation', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
+    const page = await newE2EPage({
+      html: `
     <bq-tab-group value="1">
       <bq-tab tab-id="1">Tab 1</bq-tab>
       <bq-tab tab-id="2">Tab 2</bq-tab>
       <bq-tab tab-id="3">Tab 3</bq-tab>
       <bq-tab tab-id="4">Tab 4</bq-tab>
     </bq-tab-group>
-    `);
+    `,
+    });
 
     const bqFocus = await page.spyOnEvent('bqFocus');
     const bqChange = await page.spyOnEvent('bqChange');
@@ -88,5 +93,27 @@ describe('bq-tab-group', () => {
     expect(bqFocus).toHaveReceivedEventTimes(4);
     expect(bqChange).toHaveReceivedEventTimes(3);
     expect(bqBlur).toHaveReceivedEventTimes(4);
+  });
+
+  it('should change active tab if value is change externally', async () => {
+    const page = await newE2EPage({
+      html: `
+    <bq-tab-group value="1">
+      <bq-tab tab-id="1">Tab 1</bq-tab>
+      <bq-tab tab-id="2">Tab 2</bq-tab>
+      <bq-tab tab-id="3">Tab 3</bq-tab>
+      <bq-tab tab-id="4">Tab 4</bq-tab>
+    </bq-tab-group>
+      `,
+    });
+
+    const element = await page.find('bq-tab-group');
+    element.setAttribute('value', '2');
+
+    await page.waitForChanges();
+
+    const activeElement = await page.find('bq-tab[active]');
+
+    expect(activeElement).toEqualText('Tab 2');
   });
 });
