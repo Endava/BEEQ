6c1521a2357153aeade002e46b5cba88ec309c41
Hamude Homsi <homsi.hamude@gmail.com>
2023-08-02 16:03:46 +0300

    refactor(dropdown/panel): simplify storybook

--- packages/bee-q/src/components/dropdown/_storybook/bq-dropdown.stories.tsx
+++ packages/bee-q/src/components/dropdown/_storybook/bq-dropdown.stories.tsx
@@ -6,8 +6,6 @@ import mdx from './bq-dropdown.mdx';
 
 import { PANEL_PLACEMENT } from '../../panel/bq-panel.type';
 
-const TRIGGER_ELEM_OPTIONS = ['default', 'menu', 'avatar'];
-
 const meta: Meta = {
   title: 'Components/Dropdown',
   component: 'bq-dropdown',
@@ -24,17 +22,17 @@ const meta: Meta = {
     'panel-scrollbar': { control: 'boolean' },
     // Event handlers
     bqPanelChange: { action: 'bqPanelChange' },
+    // Event handler (bq-option-list)
+    bqSelect: { action: 'bqSelect', table: { disable: true } },
     // Not part of the public API, so we don't want to expose it in the
-    text: { control: 'text', table: { disable: true } },
-    htmlTrigger: { control: 'text', table: { disable: true }, options: TRIGGER_ELEM_OPTIONS },
+    children: { control: 'text', table: { disable: true } },
   },
   args: {
     'panel-distance': 0,
     'panel-placement': 'bottom',
     'panel-open': false,
-    'panel-scrollbar': true,
-    text: 'Content in the panel',
-    htmlTrigger: '',
+    'panel-scrollbar': false,
+    children: { control: 'text', table: { disable: true } },
   },
 };
 export default meta;
@@ -42,65 +40,23 @@ export default meta;
 type Story = StoryObj;
 
 const Template = (args: Args) => {
-  const defaultTrigger = html` <bq-button
-    appearance="secondary"
-    size="medium"
-    type="button"
-    variant="standard"
-    slot="trigger"
-  >
-    <span class="flex items-center gap-1">
-      <span>Dropdown</span>
-      <bq-icon name="caret-down"></bq-icon>
-    </span>
-  </bq-button>`;
-
-  const menuTrigger = html` <bq-button
-    appearance="secondary"
-    href=""
-    size="medium"
-    target=""
-    type="button"
-    variant="standard"
-    slot="trigger"
-  >
-    <bq-icon name="dots-three-vertical"></bq-icon>
-  </bq-button>`;
-
-  const avatarTrigger = html` <bq-avatar
-    alt-text="User profile"
-    image="https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
-    label="Avatar component label"
-    initials=""
-    shape="circle"
-    size="medium"
-    slot="trigger"
-  ></bq-avatar>`;
-
-  let triggerElem = null;
-
-  switch (args.htmlTrigger) {
-    case 'menu':
-      triggerElem = menuTrigger;
-      break;
-    case 'avatar':
-      triggerElem = avatarTrigger;
-      break;
-    default:
-      triggerElem = defaultTrigger;
-  }
-
   return html`
     <bq-dropdown
       panel-distance=${args['panel-distance']}
       panel-placement=${args['panel-placement']}
       panel-open=${args['panel-open']}
+      panel-scrollbar=${args['panel-scrollbar']}
       @bqPanelChange=${args.bqPanelChange}
     >
       <!-- TRIGGER ELEMENT -->
-      ${triggerElem}
+      ${args.children}
 
-      <bq-option-list>
+      <bq-option-list @bqSelect=${args.bqSelect}>
+        <bq-option>
+          <bq-icon name="users" slot="prefix"></bq-icon>
+          <span>Users</span>
+        </bq-option>
+
         <bq-option>
           <bq-icon name="user" slot="prefix"></bq-icon>
           <span>My profile</span>
@@ -109,6 +65,11 @@ const Template = (args: Args) => {
         <bq-option>
           <bq-icon name="sliders" slot="prefix"></bq-icon>
           <span>Dashboard</span>
+        </bq-option>
+
+        <bq-option>
+          <span>Settings</span>
+          <bq-icon name="gear" slot="prefix"></bq-icon>
         </bq-option>
 
         <bq-option>
@@ -121,33 +82,88 @@ const Template = (args: Args) => {
 };
 
 export const Default: Story = {
-  render: Template,
+  render: (args: Args) => html`
+    ${Template({
+      ...args,
+      children: html`
+        <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
+          <span class="flex items-center gap-1">
+            <span>Dropdown</span>
+            <bq-icon name="caret-down"></bq-icon>
+          </span>
+        </bq-button>
+      `,
+    })}
+  `,
 };
 
 export const MenuTrigger: Story = {
-  render: Template,
-  args: {
-    htmlTrigger: 'menu',
-  },
+  render: (args: Args) => html`
+    ${Template({
+      ...args,
+      children: html`
+        <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
+          <span class="flex items-center gap-1">
+            <bq-icon name="dots-three-vertical"></bq-icon>
+          </span>
+        </bq-button>
+      `,
+    })}
+  `,
 };
 
 export const AvatarTrigger: Story = {
-  render: Template,
+  render: (args: Args) => html`
+    ${Template({
+      ...args,
+      children: html`
+        <bq-avatar
+          alt-text="User profile"
+          image="https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
+          label="Avatar component label"
+          shape="circle"
+          size="medium"
+          slot="trigger"
+        ></bq-avatar>
+      `,
+    })}
+  `,
+};
+
+export const SidePanel: Story = {
+  render: (args: Args) => html`
+    ${Template({
+      ...args,
+      children: html`
+        <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
+          <span class="flex items-center gap-1">
+            <span>Dropdown</span>
+            <bq-icon name="caret-down"></bq-icon>
+          </span>
+        </bq-button>
+      `,
+    })}
+  `,
   args: {
-    htmlTrigger: 'avatar',
+    'panel-placement': 'left-start',
   },
 };
 
-export const Top: Story = {
-  render: Template,
+export const WithScrollbar: Story = {
+  render: (args: Args) => html`
+    ${Template({
+      ...args,
+      children: html`
+        <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
+          <span class="flex items-center gap-1">
+            <span>Dropdown</span>
+            <bq-icon name="caret-down"></bq-icon>
+          </span>
+        </bq-button>
+      `,
+    })}
+  `,
   args: {
-    'panel-placement': top,
-  },
-};
-
-export const WithoutScrollbar: Story = {
-  render: Template,
-  args: {
-    'panel-scrollbar': false,
+    'panel-scrollbar': true,
   },
 };
