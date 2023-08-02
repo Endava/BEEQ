184bc7138a92830b8c0adfde6dd1993f56d0fbbf
Hamude Homsi <homsi.hamude@gmail.com>
2023-08-02 16:48:35 +0300

    refactor(dropdown/panel): rename args to 'trigger'

--- packages/bee-q/src/components/dropdown/_storybook/bq-dropdown.stories.tsx
+++ packages/bee-q/src/components/dropdown/_storybook/bq-dropdown.stories.tsx
@@ -25,67 +25,65 @@ const meta: Meta = {
     // Event handler (bq-option-list)
     bqSelect: { action: 'bqSelect', table: { disable: true } },
     // Not part of the public API, so we don't want to expose it in the
-    children: { control: 'text', table: { disable: true } },
+    trigger: { control: 'text', table: { disable: true } },
   },
   args: {
     'panel-distance': 0,
     'panel-placement': 'bottom',
     'panel-open': false,
     'panel-scrollbar': false,
-    children: { control: 'text', table: { disable: true } },
+    trigger: { control: 'text', table: { disable: true } },
   },
 };
 export default meta;
 
 type Story = StoryObj;
 
-const Template = (args: Args) => {
-  return html`
-    <bq-dropdown
-      panel-distance=${args['panel-distance']}
-      panel-placement=${args['panel-placement']}
-      panel-open=${args['panel-open']}
-      panel-scrollbar=${args['panel-scrollbar']}
-      @bqPanelChange=${args.bqPanelChange}
-    >
-      <!-- TRIGGER ELEMENT -->
-      ${args.children}
+const Template = (args: Args) => html`
+  <bq-dropdown
+    panel-distance=${args['panel-distance']}
+    panel-placement=${args['panel-placement']}
+    panel-open=${args['panel-open']}
+    panel-scrollbar=${args['panel-scrollbar']}
+    @bqPanelChange=${args.bqPanelChange}
+  >
+    <!-- TRIGGER ELEMENT -->
+    ${args.trigger}
 
-      <bq-option-list @bqSelect=${args.bqSelect}>
-        <bq-option>
-          <bq-icon name="users" slot="prefix"></bq-icon>
-          <span>Users</span>
-        </bq-option>
+    <bq-option-list @bqSelect=${args.bqSelect}>
+      <bq-option>
+        <bq-icon name="users" slot="prefix"></bq-icon>
+        <span>Users</span>
+      </bq-option>
 
-        <bq-option>
-          <bq-icon name="user" slot="prefix"></bq-icon>
-          <span>My profile</span>
-        </bq-option>
+      <bq-option>
+        <bq-icon name="user" slot="prefix"></bq-icon>
+        <span>My profile</span>
+      </bq-option>
 
-        <bq-option>
-          <bq-icon name="sliders" slot="prefix"></bq-icon>
-          <span>Dashboard</span>
-        </bq-option>
+      <bq-option>
+        <bq-icon name="sliders" slot="prefix"></bq-icon>
+        <span>Dashboard</span>
+      </bq-option>
 
-        <bq-option>
-          <span>Settings</span>
-          <bq-icon name="gear" slot="prefix"></bq-icon>
-        </bq-option>
+      <bq-option>
+        <span>Settings</span>
+        <bq-icon name="gear" slot="prefix"></bq-icon>
+      </bq-option>
 
-        <bq-option>
-          <span>Logout</span>
-          <bq-icon name="sign-out" slot="suffix"></bq-icon>
-        </bq-option>
-      </bq-option-list>
-    </bq-dropdown>
-  `;
-};
+      <bq-option>
+        <span>Logout</span>
+        <bq-icon name="sign-out" slot="suffix"></bq-icon>
+      </bq-option>
+    </bq-option-list>
+  </bq-dropdown>
+`;
 
 export const Default: Story = {
   render: (args: Args) => html`
     ${Template({
       ...args,
-      children: html`
+      trigger: html`
         <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
           <span class="flex items-center gap-1">
             <span>Dropdown</span>
@@ -101,7 +99,7 @@ export const MenuTrigger: Story = {
   render: (args: Args) => html`
     ${Template({
       ...args,
-      children: html`
+      trigger: html`
         <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
           <span class="flex items-center gap-1">
             <bq-icon name="dots-three-vertical"></bq-icon>
@@ -116,7 +114,7 @@ export const AvatarTrigger: Story = {
   render: (args: Args) => html`
     ${Template({
       ...args,
-      children: html`
+      trigger: html`
         <bq-avatar
           alt-text="User profile"
           image="https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
@@ -134,7 +132,7 @@ export const SidePanel: Story = {
   render: (args: Args) => html`
     ${Template({
       ...args,
-      children: html`
+      trigger: html`
         <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
           <span class="flex items-center gap-1">
             <span>Dropdown</span>
@@ -153,7 +151,7 @@ export const WithScrollbar: Story = {
   render: (args: Args) => html`
     ${Template({
       ...args,
-      children: html`
+      trigger: html`
         <bq-button appearance="secondary" size="medium" type="button" variant="standard" slot="trigger">
           <span class="flex items-center gap-1">
             <span>Dropdown</span>
