f5040862b2f0fabba637fc0680afde8309c7672f
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-08-02 15:12:34 +0300

    chore(Nx): update to Nx 16 (#406)

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'release'],
+      ['feat', 'fix', 'test', 'docs', 'style', 'chore', 'perf', 'refactor', 'revert', 'ci', 'build', 'wip', 'release'],
     ],
   },
 };
