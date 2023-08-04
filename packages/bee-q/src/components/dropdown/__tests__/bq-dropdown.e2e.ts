f4857cf24e6b39528a5bb519d2194eb9b357c5ee
Hamude Homsi <homsi.hamude@gmail.com>
2023-08-04 14:48:33 +0300

    fix(dropdown/panel): fix test after rm event

--- packages/bee-q/src/components/dropdown/__tests__/bq-dropdown.e2e.ts
+++ packages/bee-q/src/components/dropdown/__tests__/bq-dropdown.e2e.ts
@@ -18,21 +18,4 @@ describe('bq-dropdown', () => {
 
     expect(element.shadowRoot).not.toBeNull();
   });
-
-  it('should trigger bqPanelOpen on trigger click', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-dropdown>
-        <bq-button slot="trigger">Open</bq-button>
-        <div>some content in the panel</div>
-      </bq-dropdown>
-    `);
-
-    const bqPanelChange = await page.spyOnEvent('bqPanelChange');
-
-    const button = await page.find('bq-button >>> [part="button"]');
-    await button.click();
-
-    expect(bqPanelChange).toHaveReceivedEventTimes(1);
-  });
 });
