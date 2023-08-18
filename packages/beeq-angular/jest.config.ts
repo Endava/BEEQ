30e0992e33fb1b0823cda93f692d5e7a388ab130
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-08-18 11:55:24 +0300

    docs: all readme files and packages folder name to use `BEEQ` (#454)
    NPM packages will keep `@bee-q` momentarily until the next major release: 1.0.0

--- packages/beeq-angular/jest.config.ts
+++ packages/beeq-angular/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'bee-q-angular',
+  displayName: 'beeq-angular',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/packages/bee-q-angular',
+  coverageDirectory: '../../coverage/packages/beeq-angular',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
