a24bcf74060d402f41923248f3b768e5cce2487a
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-10-10 22:59:13 +0300

    feat(Theme): add Endava theme (#567)

--- config/theme/default/root.ts
+++ config/theme/default/root.ts
@@ -1,11 +1,11 @@
 export const DefaultRootTheme = {
   // Border Radius
-  'bq-radius--none': '0',
-  'bq-radius--xs2': '0.125rem',
-  'bq-radius--xs': '0.25rem',
-  'bq-radius--s': '0.5rem',
-  'bq-radius--m': '0.75rem',
-  'bq-radius--l': '1.5rem',
+  'bq-radius--none': '0', // 0px
+  'bq-radius--xs2': '0.125rem', // 2px
+  'bq-radius--xs': '0.25rem', // 4px
+  'bq-radius--s': '0.5rem', // 8px
+  'bq-radius--m': '0.75rem', // 12px
+  'bq-radius--l': '1.5rem', // 24px
   'bq-radius--full': '9999px',
   // Box Shadow
   'bq-box-shadow--xs': '0 2px 0 rgba(0, 0, 0, 0.016)',
@@ -13,17 +13,17 @@ export const DefaultRootTheme = {
   'bq-box-shadow--m': '0 10px 48px -16px rgba(0, 0, 0, 0.12)',
   'bq-box-shadow--l': '0 20px 58px -16px rgba(0, 0, 0, 0.16)',
   // Fonts
-  'bq-font-family--outfit': "'Outfit', sans-serif",
-  'bq-font-size--xs': '0.75rem',
-  'bq-font-size--s': '0.875rem',
-  'bq-font-size--m': '1rem',
-  'bq-font-size--l': '1.125rem',
-  'bq-font-size--xl': '1.5rem',
-  'bq-font-size--xxl': '2rem',
-  'bq-font-size--xxl2': '2.5rem',
-  'bq-font-size--xxl3': '3rem',
-  'bq-font-size--xxl4': '3.5rem',
-  'bq-font-size--xxl5': '4rem',
+  'bq-font-family': "'Outfit', sans-serif",
+  'bq-font-size--xs': '0.75rem', // 12px
+  'bq-font-size--s': '0.875rem', // 14px
+  'bq-font-size--m': '1rem', // 16px
+  'bq-font-size--l': '1.125rem', // 18px
+  'bq-font-size--xl': '1.5rem', // 24px
+  'bq-font-size--xxl': '2rem', // 32px
+  'bq-font-size--xxl2': '2.5rem', // 40px
+  'bq-font-size--xxl3': '3rem', // 48px
+  'bq-font-size--xxl4': '3.5rem', // 56px
+  'bq-font-size--xxl5': '4rem', // 64px
   'bq-font-weight--thin': '100',
   'bq-font-weight--light': '300',
   'bq-font-weight--regular': '400',
@@ -34,15 +34,15 @@ export const DefaultRootTheme = {
   'bq-font-line-height--regular': '1.5',
   'bq-font-line-height--large': '1.5',
   // Spacing
-  'bq-spacing-xs3': '0.125rem',
-  'bq-spacing-xs2': '0.25rem',
-  'bq-spacing-xs': '0.5rem',
-  'bq-spacing-s': '0.75rem',
-  'bq-spacing-m': '1rem',
-  'bq-spacing-l': '1.5rem',
-  'bq-spacing-xl': '2rem',
-  'bq-spacing-xxl': '2.5rem',
-  'bq-spacing-xxl2': '3.5rem',
-  'bq-spacing-xxl3': '4rem',
-  'bq-spacing-xxl4': '4.5rem',
+  'bq-spacing-xs3': '0.125rem', // 2px
+  'bq-spacing-xs2': '0.25rem', // 4px
+  'bq-spacing-xs': '0.5rem', // 8px
+  'bq-spacing-s': '0.75rem', // 12px
+  'bq-spacing-m': '1rem', // 16px
+  'bq-spacing-l': '1.5rem', // 24px
+  'bq-spacing-xl': '2rem', // 32px
+  'bq-spacing-xxl': '2.5rem', // 40px
+  'bq-spacing-xxl2': '3.5rem', // 56px
+  'bq-spacing-xxl3': '4rem', // 64px
+  'bq-spacing-xxl4': '4.5rem', // 72px
 };
