771915472b33f1c1c2f1f80315cdb3184e4b500c
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-10-17 13:59:21 +0300

    chore(deps): update available patch and minor devDependencies (#583)

--- packages/beeq/src/components/input/__tests__/bq-input.e2e.ts
+++ packages/beeq/src/components/input/__tests__/bq-input.e2e.ts
@@ -2,75 +2,80 @@ import { newE2EPage } from '@stencil/core/testing';
 
 describe('bq-input', () => {
   it('should render', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-input></bq-input>');
+    const page = await newE2EPage({
+      html: '<bq-input></bq-input>',
+    });
 
     const bqInputElem = await page.find('bq-input');
     expect(bqInputElem).toHaveClass('hydrated');
   });
 
   it('should have shadow root', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-input></bq-input>');
+    const page = await newE2EPage({
+      html: '<bq-input></bq-input>',
+    });
 
     const bqInputElem = await page.find('bq-input');
     expect(bqInputElem.shadowRoot).not.toBeNull();
   });
 
   it('should render with prefix icon', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input>
-        <bq-icon name="user-circle" slot="prefix"></bq-icon>
-      </bq-input>
-    `);
+    const page = await newE2EPage({
+      html: `
+        <bq-input>
+          <bq-icon name="user-circle" slot="prefix"></bq-icon>
+        </bq-input>
+      `,
+    });
 
     const prefixContainerElem = await page.find('bq-input >>> .bq-input--control__prefix');
     expect(prefixContainerElem).not.toHaveClass('hidden');
   });
 
   it('should render with suffix icon', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input>
-        <bq-icon name="gear" slot="suffix"></bq-icon>
-      </bq-input>
-    `);
+    const page = await newE2EPage({
+      html: `
+        <bq-input>
+          <bq-icon name="gear" slot="suffix"></bq-icon>
+        </bq-input>
+      `,
+    });
 
     const suffixContainerElem = await page.find('bq-input >>> .bq-input--control__suffix');
     expect(suffixContainerElem).not.toHaveClass('hidden');
   });
 
   it('should render with label content', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input>
-        <label slot="label">Input label</label>
-      </bq-input>
-    `);
+    const page = await newE2EPage({
+      html: `
+        <bq-input>
+          <label slot="label">Input label</label>
+        </bq-input>
+      `,
+    });
 
     const labelContainerElem = await page.find('bq-input >>> .bq-input--label');
     expect(labelContainerElem).not.toHaveClass('hidden');
   });
 
   it('should render with helper content', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input>
-        <span slot="helper-text">Helper text</span>
-      </bq-input>
-    `);
+    const page = await newE2EPage({
+      html: `
+        <bq-input>
+          <span slot="helper-text">Helper text</span>
+        </bq-input>
+      `,
+    });
 
     const helperContainerElem = await page.find('bq-input >>> .bq-input--helper-text');
     expect(helperContainerElem).not.toHaveClass('hidden');
   });
 
   it('should write and emit change event', async () => {
-    const inputValue = 'Hello World!';
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input></bq-input>
-    `);
+    const inputValue = 'Hello';
+    const page = await newE2EPage({
+      html: '<bq-input></bq-input>',
+    });
 
     const bqChange = await page.spyOnEvent('bqChange');
 
@@ -88,18 +93,17 @@ describe('bq-input', () => {
   });
 
   it('should write and emit input event', async () => {
-    const inputValue = 'Hello World!';
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input></bq-input>
-    `);
+    const inputValue = 'Hello';
+    const page = await newE2EPage({
+      html: '<bq-input></bq-input>',
+    });
 
     const bqInput = await page.spyOnEvent('bqInput');
 
     const bqInputElem = await page.find('bq-input');
-    const nativeInputElem = await page.find('bq-input >>> .bq-input--control__input');
 
-    await nativeInputElem.type(inputValue);
+    await page.focus('bq-input >>> .bq-input--control__input');
+    await page.keyboard.type(inputValue);
     await page.waitForChanges();
 
     expect(await bqInputElem.getProperty('value')).toBe(inputValue);
@@ -107,16 +111,18 @@ describe('bq-input', () => {
   });
 
   it('should clear the value and emit clear event', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-input value="Hello World!"></bq-input>
-    `);
+    const inputValue = 'Hello';
+    const page = await newE2EPage({
+      html: `
+        <bq-input value="${inputValue}"></bq-input>
+      `,
+    });
 
     const bqClear = await page.spyOnEvent('bqClear');
 
     const bqInputElem = await page.find('bq-input');
     const nativeInputElem = await page.find('bq-input >>> .bq-input--control__input');
-    expect(await nativeInputElem.getProperty('value')).toBe('Hello World!');
+    expect(await nativeInputElem.getProperty('value')).toBe(inputValue);
 
     await nativeInputElem.focus();
     await page.waitForChanges();
