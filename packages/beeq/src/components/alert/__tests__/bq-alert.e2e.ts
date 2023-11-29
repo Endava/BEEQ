7951e935f9cc85d5fa7390920bec9c4f46f46029
clungan <catalin.lungan@endava.com>
2023-11-29 15:59:17 +0200

    feat(Alert): refactor tests

--- packages/beeq/src/components/alert/__tests__/bq-alert.e2e.ts
+++ packages/beeq/src/components/alert/__tests__/bq-alert.e2e.ts
@@ -2,24 +2,21 @@ import { newE2EPage } from '@stencil/core/testing';
 
 describe('bq-alert', () => {
   it('should render', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-alert></bq-alert>');
+    const page = await newE2EPage({ html: `<bq-alert></bq-alert>` });
 
     const element = await page.find('bq-alert');
     expect(element).toHaveClass('hydrated');
   });
 
   it('should have shadow root', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-alert></bq-alert>');
+    const page = await newE2EPage({ html: `<bq-alert></bq-alert>` });
 
     const element = await page.find('bq-alert');
     expect(element.shadowRoot).not.toBeNull();
   });
 
   it('should render as hidden', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-alert></bq-alert>');
+    const page = await newE2EPage({ html: `<bq-alert></bq-alert>` });
 
     const element = await page.find('bq-alert');
     expect(element).toEqualAttribute('aria-hidden', 'true');
@@ -27,8 +24,7 @@ describe('bq-alert', () => {
   });
 
   it('should render as hidden with `open="false"`', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-alert open="false"></bq-alert>');
+    const page = await newE2EPage({ html: `<bq-alert open="false"></bq-alert>` });
 
     const element = await page.find('bq-alert');
     expect(element).toEqualAttribute('aria-hidden', 'true');
@@ -36,8 +32,7 @@ describe('bq-alert', () => {
   });
 
   it('should render as open', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-alert open></bq-alert>');
+    const page = await newE2EPage({ html: `<bq-alert open></bq-alert>` });
 
     const element = await page.find('bq-alert');
     expect(element).not.toEqualAttribute('aria-hidden', 'true');
@@ -45,8 +40,7 @@ describe('bq-alert', () => {
   });
 
   it('should render as open with `open="true"`', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-alert open="true"></bq-alert>');
+    const page = await newE2EPage({ html: `<bq-alert open="true"></bq-alert>` });
 
     const element = await page.find('bq-alert');
     expect(element).not.toEqualAttribute('aria-hidden', 'true');
@@ -54,49 +48,45 @@ describe('bq-alert', () => {
   });
 
   it('should render basic alert', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-alert>
-        Alert title
-        <span slot="body">You have a new alert message</span>
-      </bq-alert>
-    `);
+    const page = await newE2EPage({
+      html: `
+    <bq-alert>
+      Alert title
+      <span slot="body">You have a new alert message</span>
+    </bq-alert>
+  `,
+    });
 
     const description = await page.find('bq-alert >>> slot[name="body"]');
     expect(description).not.toBeNull();
   });
 
   it('should show alert with icon', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-alert type="info">Alert title</bq-alert>
-    `);
+    const page = await newE2EPage({ html: `<bq-alert type="info">Alert title</bq-alert>` });
 
     const iconHolder = await page.find('bq-alert >>> [part="icon-outline"]');
     expect(iconHolder).not.toBeNull();
   });
 
   it('should show alert with close button', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-alert type="info">Alert title</bq-alert>
-    `);
+    const page = await newE2EPage({ html: `<bq-alert type="info">Alert title</bq-alert>` });
 
     const iconHolder = await page.find('bq-alert >>> [part="btn-close"]');
     expect(iconHolder).not.toBeNull();
   });
 
   it('should show alert footer', async () => {
-    const page = await newE2EPage();
-    await page.setContent(`
-      <bq-alert>
-        Alert title
-        <div slot="footer">
-          <bq-button appearance="primary" type="button" variant="standard">Button</bq-button>
-          <bq-button appearance="secondary" variant="standard">Button</bq-button>
-        </div>
-      </bq-alert>',
-    `);
+    const page = await newE2EPage({
+      html: `
+    <bq-alert>
+    Alert title
+    <div slot="footer">
+      <bq-button appearance="primary" type="button" variant="standard">Button</bq-button>
+      <bq-button appearance="secondary" variant="standard">Button</bq-button>
+    </div>
+  </bq-alert>
+    `,
+    });
 
     const footerSlot = await page.find('bq-alert >>> slot[name="footer"]');
     expect(footerSlot).not.toBeNull();
