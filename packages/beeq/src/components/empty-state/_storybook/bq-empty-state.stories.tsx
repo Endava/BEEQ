ac82fa67ac62ffd8c3e6c5359b783c91c6bbea41
Lungan Catalin <98550729+Cata1989@users.noreply.github.com>
2023-12-07 17:50:29 +0200

    docs(Empty State): remove unused constants (#696)

--- packages/beeq/src/components/empty-state/_storybook/bq-empty-state.stories.tsx
+++ packages/beeq/src/components/empty-state/_storybook/bq-empty-state.stories.tsx
@@ -23,44 +23,6 @@ export default meta;
 
 type Story = StoryObj;
 
-const Template = (args: Args) => html` <bq-empty-state size=${args.size}> Title </bq-empty-state> `;
-
-const TemplateWithBody = (args: Args) => html`
-  <div class="flex flex-row gap-20">
-    <bq-empty-state size=${args.size}>
-      Title
-      <span slot="body"> Description </span>
-    </bq-empty-state>
-    <bq-empty-state size=${args.size}>
-      Title <span slot="body"> Description <a class="bq-link" href="https://example.com">Link</a> </span>
-    </bq-empty-state>
-  </div>
-`;
-
-const TemplateWithCTA = (args: Args) => html`
-  <div class="flex flex-row gap-20">
-    <bq-empty-state size=${args.size}>
-      Title <span slot="body"> Description <a class="bq-link" href="https://example.com">Link</a> </span>
-      <div class="flex gap-xs" slot="footer">
-        <bq-button appearance="primary" size="small"> Button </bq-button>
-      </div>
-    </bq-empty-state>
-    <bq-empty-state size=${args.size}>
-      Title <span slot="body"> Description <a class="bq-link" href="https://example.com">Link</a> </span>
-      <div class="flex gap-xs" slot="footer">
-        <bq-button size="small" variant="ghost"> Button </bq-button>
-      </div>
-    </bq-empty-state>
-    <bq-empty-state size=${args.size}>
-      Title <span slot="body"> Description <a class="bq-link" href="https://example.com">Link</a> </span>
-      <div class="flex gap-xs" slot="footer">
-        <bq-button size="small" variant="ghost"> Button </bq-button>
-        <bq-button appearance="primary" size="small"> Button </bq-button>
-      </div>
-    </bq-empty-state>
-  </div>
-`;
-
 export const Default: Story = {
   render: (args: Args) => html` <bq-empty-state size=${args.size}> Title </bq-empty-state> `,
 };
