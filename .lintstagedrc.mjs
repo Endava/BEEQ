b7595832a915c3a856a405135009a7690be251db
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2023-12-15 12:47:31 +0200

    fix(Textarea): export missing shadow DOM parts (#708)

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,9 @@
 export default {
   // Global prettier
   '{apps,packages,tools}/**/*.{js,json,ts,tsx,scss}': (files) => `nx format:write --files=${files.join(',')}`,
-  // BEEQ linters
-  'packages/beeq/src/**/*.*': "npx nx affected -t lint test --exclude='*,!tag:publishable' -- --fix --findRelatedTests",
+  // BEEQ linters and unit tests
+  'packages/beeq/src/**/*.*': [
+    "npx nx affected -t lint --exclude='*,!tag:publishable' -- --fix=true",
+    "npx nx affected -t test --exclude='*,!tag:publishable' -- --findRelatedTests --passWithNoTests",
+  ],
 };
