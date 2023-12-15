fe5fc72c34957779b7965490c7a8369c4fed8182
Lungan Catalin <catalin.lungan@endava.com>
2023-12-15 14:08:06 +0200

    feat(Tag): add e2e tests for bq-tag

--- packages/beeq/src/components/tag/__tests__/bq-tag.e2e.ts
+++ packages/beeq/src/components/tag/__tests__/bq-tag.e2e.ts
@@ -1,5 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
 
+import { computedStyle } from '../../../shared/test-utils';
+
 describe('bq-tag', () => {
   it('should render', async () => {
     const page = await newE2EPage({
@@ -17,5 +19,100 @@ describe('bq-tag', () => {
     const element = await page.find('bq-tag');
 
     expect(element.shadowRoot).not.toBeNull();
+  });
+
+  it('should render as hidden', async () => {
+    const page = await newE2EPage({
+      html: `<bq-tag></bq-tag>`,
+    });
+
+    const element = await page.find('bq-tag');
+    expect(element).toEqualAttribute('aria-hidden', 'true');
+    expect(element).toHaveClass('is-hidden');
+  });
+
+  it('should render as hidden with `open="false"`', async () => {
+    const page = await newE2EPage({
+      html: `<bq-tag open="false"></bq-tag>`,
+    });
+
+    const element = await page.find('bq-tag');
+    expect(element).toEqualAttribute('aria-hidden', 'true');
+    expect(element).toHaveClass('is-hidden');
+  });
+
+  it('should render as open', async () => {
+    const page = await newE2EPage({
+      html: `<bq-tag open></bq-tag>`,
+    });
+
+    const element = await page.find('bq-tag');
+    expect(element).toEqualAttribute('aria-hidden', 'false');
+    expect(element).not.toHaveClass('is-hidden');
+  });
+
+  it('should render as open with `open="true"`', async () => {
+    const page = await newE2EPage({
+      html: `<bq-tag open="true"></bq-tag>`,
+    });
+
+    const element = await page.find('bq-tag');
+    expect(element).toEqualAttribute('aria-hidden', 'false');
+    expect(element).not.toHaveClass('is-hidden');
+  });
+
+  it('should render a removable tag component', async () => {
+    const page = await newE2EPage({
+      html: `<bq-tag removable>Tag</bq-tag>`,
+    });
+
+    const element = await page.find('bq-tag >>> bq-icon[name="x-circle"]');
+    expect(element).not.toBeNull();
+  });
+
+  it('should render a basic tag without icon', async () => {
+    const page = await newE2EPage({
+      html: `<bq-tag>Tag</bq-tag>`,
+    });
+
+    const element = await page.find('bq-tag >>> slot');
+
+    expect(element).not.toBeNull();
+  });
+
+  it('should render a tag with a prefix (icon)', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-tag>
+          <span slot="prefix">
+            <bq-icon name="star"></bq-icon>
+          </span>
+          Tag
+        </bq-tag>
+      `,
+    });
+
+    const prefixSlot = await page.find('bq-tag >>> slot[name="prefix"]');
+    expect(prefixSlot).not.toBeNull();
+  });
+
+  it('should respect design style', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-tag size="extra_small">Tag</tag>
+        <bq-tag size="small">Tag</tag>
+        <bq-tag size="medium">Tag</tag>
+      `,
+    });
+
+    const styleProps = ['padding'] as const;
+
+    const extra_smallStyle = await computedStyle(page, 'bq-tag[size="extra_small"] >>> [part="wrapper"]', styleProps);
+    const smallStyle = await computedStyle(page, 'bq-tag[size="small"] >>> [part="wrapper"]', styleProps);
+    const mediumStyle = await computedStyle(page, 'bq-tag[size="medium"] >>> [part="wrapper"]', styleProps);
+
+    expect(extra_smallStyle).toEqual({ padding: '2px 8px' });
+    expect(smallStyle).toEqual({ padding: '2px 8px' });
+    expect(mediumStyle).toEqual({ padding: '4px 12px' });
   });
 });
