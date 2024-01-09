35af144da0827df43e3052969612f92e798c2296
Dabiel González Ramos <dabiel.ramos@endava.com>
2024-01-09 15:46:54 +0200

    chore(deps): sync package-lock.json

--- packages/beeq/src/components/tag/bq-tag.types.ts
+++ packages/beeq/src/components/tag/bq-tag.types.ts
@@ -2,7 +2,7 @@ export const TAG_SIZE = ['xsmall', 'small', 'medium'] as const;
 export type TTagSize = (typeof TAG_SIZE)[number];
 
 export const TAG_COLOR = ['error', 'gray', 'info', 'success', 'warning'] as const;
-export type TTagColor = (typeof TAG_COLOR)[number] | (string & object);
+export type TTagColor = (typeof TAG_COLOR)[number];
 
 export const TAG_VARIANT = ['outline', 'filled'] as const;
 export type TTagVariant = (typeof TAG_VARIANT)[number];
