7db3e746215e2588bc72ca9d1cd1bb0f12e6d839
Lungan Catalin <catalin.lungan@endava.com>
2024-01-09 14:14:43 +0200

    fix(cssVariables): fix minor typo

--- packages/beeq/src/shared/utils/cssVariables.ts
+++ packages/beeq/src/shared/utils/cssVariables.ts
@@ -4,7 +4,7 @@ const CSSVALUEPREFIX = '--bq-';
  * Get CSS custom property variable of a given color string value
  *
  * @param {string} colorName - Valid string value.
- * @return {string} The correspongin CSS custom property variable
+ * @return {string} The corresponding CSS custom property variable
  */
 export function getColorCSSVariable(colorName: string): string {
   const token = `${CSSVALUEPREFIX}${colorName}`;
