6cff3718904194f60e11c3417a50bf000b644576
Lungan Catalin <catalin.lungan@endava.com>
2024-02-05 20:30:06 +0200

    feat(Card): add mdx docs and e2e tests for default and minimal type render

--- packages/beeq/src/components/card/__tests__/bq-card.e2e.ts
+++ packages/beeq/src/components/card/__tests__/bq-card.e2e.ts
@@ -1,5 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
 
+import { computedStyle } from '../../../shared/test-utils';
+
 describe('bq-card', () => {
   it('should render', async () => {
     const page = await newE2EPage({
@@ -15,6 +17,36 @@ describe('bq-card', () => {
       html: '<bq-card></bq-card>',
     });
     const element = await page.find('bq-card');
+
+    expect(element.shadowRoot).not.toBeNull();
+  });
+
+  it('should render default type', async () => {
+    const page = await newE2EPage({
+      html: '<bq-card type="default"></bq-card>',
+    });
+    const element = await page.find('bq-card');
+
+    const styleProps = ['padding'] as const;
+
+    const minimalStyle = await computedStyle(page, 'bq-card[type="default"] >>> [part="wrapper"]', styleProps);
+
+    expect(minimalStyle).toEqual({ padding: '24px' });
+
+    expect(element.shadowRoot).not.toBeNull();
+  });
+
+  it('should render minimal type', async () => {
+    const page = await newE2EPage({
+      html: '<bq-card type="minimal"></bq-card>',
+    });
+    const element = await page.find('bq-card');
+
+    const styleProps = ['padding'] as const;
+
+    const minimalStyle = await computedStyle(page, 'bq-card[type="minimal"] >>> [part="wrapper"]', styleProps);
+
+    expect(minimalStyle).toEqual({ padding: '0px' });
 
     expect(element.shadowRoot).not.toBeNull();
   });
