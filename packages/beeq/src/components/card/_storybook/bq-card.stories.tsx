3827e3fc00173aeaad454c1a8b3c37d802c3daab
Lungan Catalin <catalin.lungan@endava.com>
2024-02-05 11:11:14 +0200

    feat(Card): add separate story for each example

--- packages/beeq/src/components/card/_storybook/bq-card.stories.tsx
+++ packages/beeq/src/components/card/_storybook/bq-card.stories.tsx
@@ -1,5 +1,6 @@
 import type { Args, Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit-html';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 import mdx from './bq-card.mdx';
 import { CARD_TYPE } from '../bq-card.types';
@@ -14,6 +15,7 @@ const meta: Meta = {
   },
   argTypes: {
     type: { control: 'select', options: [...CARD_TYPE] },
+    content: { control: 'text', table: { disable: true } },
   },
   args: {
     type: 'default',
@@ -24,8 +26,13 @@ export default meta;
 type Story = StoryObj;
 
 const Template = (args: Args) => html`
-  <div class="flex flex-row gap-m items-end">
-    <bq-card type=${args.type}>
+  <div class="max-w-sm"><bq-card type=${ifDefined(args.type)}>${ifDefined(args.content)}</bq-card></div>
+`;
+
+export const Default: Story = {
+  render: Template,
+  args: {
+    content: html`
       <div class="flex flex-col gap-6">
         <div>
           <div class="flex flex-row items-center gap-xs2">
@@ -42,58 +49,72 @@ const Template = (args: Args) => html`
           <p class="text-text-secondary">avg resolution</p>
         </div>
       </div>
-    </bq-card>
-    <bq-card type=${args.type}>
+    `,
+  },
+};
+
+export const Feature_Highlights: Story = {
+  render: Template,
+  args: {
+    content: html`
       <div class="flex flex-col font-medium gap-m">
-          <h6 class="font-bold">Title</h6>
-          <div class="flex flex-col gap-s">
-            <div class="flex flex-row items-center gap-xs">
-              <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-              Lorem Ipsum is simply dummy text 
-            </div>
-            <div class="flex flex-row items-center gap-xs">
-              <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-              Lorem Ipsum is simply dummy text 
-            </div>
-            <div class="flex flex-row items-center gap-xs">
-              <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-              Lorem Ipsum is simply dummy text 
-            </div>
-            <div class="flex flex-row items-center gap-xs">
-              <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-              Lorem Ipsum is simply dummy text 
-            </div>
+        <h6 class="font-bold">Title</h6>
+        <div class="flex flex-col gap-s">
+          <div class="flex flex-row items-center gap-xs">
+            <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
+            Lorem Ipsum is simply dummy text 
+          </div>
+          <div class="flex flex-row items-center gap-xs">
+            <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
+            Lorem Ipsum is simply dummy text 
+          </div>
+          <div class="flex flex-row items-center gap-xs">
+            <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
+            Lorem Ipsum is simply dummy text 
+          </div>
+          <div class="flex flex-row items-center gap-xs">
+            <bq-icon name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
+            Lorem Ipsum is simply dummy text 
           </div>
         </div>
-    </bq-card>
-    <bq-card type=${args.type}>
-      <div class="flex flex-row gap-m">
-          <bq-icon color="text--brand" size="56" name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-          <div>
-            <div class="flex flex-row items-center gap-xs2">
-              <span class="text-text-secondary">Title</span> <bq-icon color="text--brand" size="16" name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-            </div>
-            <div class="flex items-end gap-xs">
-              <h4>194</h4>
-              <span class="text-text-success">+24%</span>
-              <span class="text-text-secondary">than last mo.</span>
-            </div>
-          </div>
       </div>
-    </bq-card>
-    <bq-card type=${args.type}>
+    `,
+  },
+};
+
+export const Performance_Overview: Story = {
+  render: Template,
+  args: {
+    content: html`
+      <div class="flex flex-row gap-m">
+        <bq-icon color="text--brand" size="56" name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
+        <div>
+          <div class="flex flex-row items-center gap-xs2">
+            <span class="text-text-secondary">Title</span> <bq-icon color="text--brand" size="16" name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
+          </div>
+          <div class="flex items-end gap-xs">
+            <h4>194</h4>
+            <span class="text-text-success">+24%</span>
+            <span class="text-text-secondary">than last mo.</span>
+          </div>
+        </div>
+      </div>
+    `,
+  },
+};
+
+export const Detailed_Content: Story = {
+  render: Template,
+  args: {
+    content: html`
       <div class="flex flex-col items-start gap-m">
         <bq-icon color="text--brand" size="56" name="star" weight="bold" part="icon" exportparts="base,svg" /></bq-icon>
-        <div class="flex flex-col gap-xs2 max-w-sm">
+        <div class="flex flex-col gap-xs2">
           <h6 class="font-bold">Title</h6>
           <p class="text-text-secondary items-stretch">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
         </div>
         <bq-button appearance="link">Button</bq-button>
       </div>
-    </bq-card>
-  </div>
-`;
-export const Default: Story = {
-  render: Template,
-  args: {},
+    `,
+  },
 };
