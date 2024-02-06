7aa376d8ae4ef84504e5c4cb783befa051098289
Lungan Catalin <catalin.lungan@endava.com>
2024-02-06 15:55:07 +0200

    feat(Notification): introduce flexible border property to facilitate dynamic border-radius changes

--- packages/beeq/src/components/notification/bq-notification.types.ts
+++ packages/beeq/src/components/notification/bq-notification.types.ts
@@ -1,2 +1,5 @@
 export const NOTIFICATION_TYPE = ['error', 'info', 'neutral', 'success', 'warning'] as const;
 export type TNotificationType = (typeof NOTIFICATION_TYPE)[number];
+
+export const NOTIFICATION_BORDER_RADIUS = ['none', 'xs2', 'xs', 's', 'm', 'l', 'full'] as const;
+export type TNotificationBorderRadius = (typeof NOTIFICATION_BORDER_RADIUS)[number];
