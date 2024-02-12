eff4597e292e5bd4907cb08ba3f03c0ace10f195
Bogdan Bosca <109202804+endv-bogdanb@users.noreply.github.com>
2024-02-12 14:25:08 +0200

    test(Accordion): add e2e tests for accordion-group (#828)

--- packages/beeq/src/components/accordion-group/__tests__/bq-accordion-group.e2e.ts
+++ packages/beeq/src/components/accordion-group/__tests__/bq-accordion-group.e2e.ts
@@ -2,8 +2,14 @@ import { newE2EPage } from '@stencil/core/testing';
 
 describe('bq-accordion-group', () => {
   it('should render', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-accordion-group></bq-accordion-group>');
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
 
     const element = await page.find('bq-accordion-group');
 
@@ -11,11 +17,130 @@ describe('bq-accordion-group', () => {
   });
 
   it('should have shadow root', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<bq-accordion-group></bq-accordion-group>');
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
 
     const element = await page.find('bq-accordion-group');
 
     expect(element.shadowRoot).not.toBeNull();
+  });
+
+  it('should expand all accordions', async () => {
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group expand-all>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
+
+    const elements = await page.findAll('bq-accordion[expanded]');
+
+    expect(elements).toHaveLength(3);
+  });
+
+  it('should colapse all accordions', async () => {
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group expand-all>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
+
+    const element = await page.find('bq-accordion-group');
+    element.setProperty('expandAll', false);
+    await page.waitForChanges();
+
+    const elements = await page.findAll('bq-accordion[expanded]');
+
+    expect(elements).toHaveLength(0);
+  });
+
+  it('should emit bqClick on accordion click', async () => {
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
+
+    const bqFocus = await page.spyOnEvent('bqFocus');
+    const bqClick = await page.spyOnEvent('bqClick');
+    const bqBlur = await page.spyOnEvent('bqBlur');
+
+    const element = await page.find('bq-accordion');
+    await element.click();
+
+    expect(await element.getProperty('expanded')).toBe(true);
+    expect(bqFocus).toHaveReceivedEventTimes(1);
+    expect(bqClick).toHaveReceivedEventTimes(1);
+    expect(bqBlur).toHaveReceivedEventTimes(0);
+  });
+
+  it('should have only one accordion expanded', async () => {
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group expand-all>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
+
+    const bqFocus = await page.spyOnEvent('bqFocus');
+    const bqClick = await page.spyOnEvent('bqClick');
+    const bqBlur = await page.spyOnEvent('bqBlur');
+
+    await page.click('bq-accordion', { count: 2 });
+    await page.waitForChanges();
+
+    const elements = await page.findAll('bq-accordion[expanded]');
+
+    expect(elements).toHaveLength(1);
+    expect(bqFocus).toHaveReceivedEventTimes(1);
+    expect(bqClick).toHaveReceivedEventTimes(2);
+    expect(bqBlur).toHaveReceivedEventTimes(0);
+  });
+
+  it('should be keyboard navigable', async () => {
+    const page = await newE2EPage({
+      html: `
+      <bq-accordion-group expand-all>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+        <bq-accordion></bq-accordion>
+      </bq-accordion-group>
+      `,
+    });
+
+    const bqFocus = await page.spyOnEvent('bqFocus');
+    const bqClick = await page.spyOnEvent('bqClick');
+    const bqBlur = await page.spyOnEvent('bqBlur');
+
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+
+    const focusedTagName = await page.evaluate(() => document.activeElement.tagName.toLocaleLowerCase());
+
+    expect(bqFocus).toHaveReceivedEventTimes(3);
+    expect(bqClick).toHaveReceivedEventTimes(0);
+    expect(bqBlur).toHaveReceivedEventTimes(2);
+    expect(focusedTagName).toEqual('bq-accordion');
   });
 });
