3b81a4eb7b198dc2498459689a6d18210060f1ac
Lungan Catalin <catalin.lungan@endava.com>
2024-02-15 15:06:43 +0200

    refactor(tokens): update mapped colors in light file with root values for default theme

--- packages/beeq-tailwindcss/src/theme/default/light.ts
+++ packages/beeq-tailwindcss/src/theme/default/light.ts
@@ -2,193 +2,193 @@ export const DefaultLightTheme = {
   /* -------------------------------------------------------------------------- */
   /*                              Background colors                             */
   /* -------------------------------------------------------------------------- */
-  'bq-background--primary': 'var(--bq-neutral-white)',
-  'bq-background--secondary': 'var(--bq-grey-100)',
-  'bq-background--tertiary': 'var(--bq-grey-200)',
-  'bq-background--alt': 'var(--bq-grey-300)',
-  'bq-background--inverse': 'var(--bq-grey-900)',
-  'bq-background--brand': 'var(--bq-iris-600)',
-  'bq-background--overlay': 'var(--bq-grey-900)', // with opacity: 60%
+  'bq-background--primary': 'var(--bq-white)',
+  'bq-background--secondary': 'var(--bq-neutral-100)',
+  'bq-background--tertiary': 'var(--bq-neutral-200)',
+  'bq-background--alt': 'var(--bq-neutral-300)',
+  'bq-background--inverse': 'var(--bq-neutral-900)',
+  'bq-background--brand': 'var(--bq-brand)',
+  'bq-background--overlay': 'var(--bq-neutral-900)', // with opacity: 60%
   /* -------------------------------------------------------------------------- */
   /*                                 Data colors                                */
   /* -------------------------------------------------------------------------- */
-  'bq-data--blue': 'var(--bq-blue-600)',
-  'bq-data--brand': 'var(--bq-iris-600)',
-  'bq-data--corai': 'var(--bq-corai-600)',
-  'bq-data--cyan': 'var(--bq-cyan-600)',
-  'bq-data--green': 'var(--bq-green-600)',
-  'bq-data--grey': 'var(--bq-grey-600)',
-  'bq-data--inverse': 'var(--bq-grey-950)',
+  'bq-data--blue': 'var(--bq-data-09)',
+  'bq-data--brand': 'var(--bq-brand)',
+  'bq-data--corai': 'var(--bq-corai-600)', // not found in root
+  'bq-data--cyan': 'var(--bq-data-04)',
+  'bq-data--green': 'var(--bq-green-600)', // not found in root
+  'bq-data--grey': 'var(--bq-data-10)',
+  'bq-data--inverse': 'var(--bq-neutral-950)',
   'bq-data--lime': 'var(--bq-lime-600)',
-  'bq-data--magenta': 'var(--bq-magenta-600)',
-  'bq-data--orange': 'var(--bq-orange-600)',
-  'bq-data--purple': 'var(--bq-purple-600)',
-  'bq-data--red': 'var(--bq-red-600)',
-  'bq-data--sky': 'var(--bq-sky-600)',
-  'bq-data--teal': 'var(--bq-teal-600)',
-  'bq-data--yellow': 'var(--bq-yellow-600)',
+  'bq-data--magenta': 'var(--bq-data-03)',
+  'bq-data--orange': 'var(--bq-data-06)',
+  'bq-data--purple': 'var(--bq-accent)',
+  'bq-data--red': 'var(--bq-danger)',
+  'bq-data--sky': 'var(--bq-sky-600)', // not found in root
+  'bq-data--teal': 'var(--bq-data-05)',
+  'bq-data--yellow': 'var(--bq-data-07)',
   /* -------------------------------------------------------------------------- */
   /*                                    Focus                                   */
   /* -------------------------------------------------------------------------- */
-  'bq-focus': 'var(--bq-iris-500)',
+  'bq-focus': 'var(--bq-iris-500)', // not found in root
   /* -------------------------------------------------------------------------- */
   /*                                Icons colors                                */
   /* -------------------------------------------------------------------------- */
   /** Primary */
-  'bq-icon--primary': 'var(--bq-grey-800)',
-  'bq-icon--primary-hover': 'var(--bq-grey-700)',
-  'bq-icon--primary-active': 'var(--bq-grey-900)',
-  'bq-icon--primary-disabled': 'var(--bq-grey-500)',
-  'bq-icon--primary-alt': 'var(--bq-neutral-white)',
-  'bq-icon--primary-alt-disabled': 'var(--bq-grey-200)',
+  'bq-icon--primary': 'var(--bq-neutral-800)',
+  'bq-icon--primary-hover': 'var(--bq-neutral-700)',
+  'bq-icon--primary-active': 'var(--bq-neutral-900)',
+  'bq-icon--primary-disabled': 'var(--bq-neutral-500)',
+  'bq-icon--primary-alt': 'var(--bq-white)',
+  'bq-icon--primary-alt-disabled': 'var(--bq-neutral-200)',
   /** Secondary */
-  'bq-icon--secondary': 'var(--bq-grey-600)',
-  'bq-icon--secondary-hover': 'var(--bq-grey-500)',
-  'bq-icon--secondary-active': 'var(--bq-grey-700)',
-  'bq-icon--secondary-disabled': 'var(--bq-grey-400)',
+  'bq-icon--secondary': 'var(--bq-neutral-600)',
+  'bq-icon--secondary-hover': 'var(--bq-neutral-500)',
+  'bq-icon--secondary-active': 'var(--bq-neutral-700)',
+  'bq-icon--secondary-disabled': 'var(--bq-neutral-400)',
   /** Inverse */
-  'bq-icon--inverse': 'var(--bq-grey-50)',
-  'bq-icon--inverse-disabled': 'var(--bq-grey-100)',
+  'bq-icon--inverse': 'var(--bq-neutral-50)',
+  'bq-icon--inverse-disabled': 'var(--bq-neutral-100)',
   /** Brand */
-  'bq-icon--brand': 'var(--bq-iris-600)',
-  'bq-icon--brand-hover': 'var(--bq-iris-500)',
-  'bq-icon--brand-active': 'var(--bq-iris-700)',
-  'bq-icon--brand-disabled': 'var(--bq-iris-400)',
+  'bq-icon--brand': 'var(--bq-brand)',
+  'bq-icon--brand-hover': 'var(--bq-iris-500)', // not found in root
+  'bq-icon--brand-active': 'var(--bq-iris-700)', // not found in root
+  'bq-icon--brand-disabled': 'var(--bq-iris-400)', // not found in root
   /** Feedback */
-  'bq-icon--info': 'var(--bq-blue-600)',
-  'bq-icon--success': 'var(--bq-teal-600)',
-  'bq-icon--warning': 'var(--bq-gold-600)',
-  'bq-icon--danger': 'var(--bq-red-600)',
+  'bq-icon--info': 'var(--bq-brand)',
+  'bq-icon--success': 'var(--bq-success)',
+  'bq-icon--warning': 'var(--bq-warning)',
+  'bq-icon--danger': 'var(--bq-danger)',
   /* -------------------------------------------------------------------------- */
   /*                                Stroke colors                               */
   /* -------------------------------------------------------------------------- */
   /** Primary */
-  'bq-stroke--primary': 'var(--bq-grey-200)',
-  'bq-stroke--primary-hover': 'var(--bq-grey-100)',
-  'bq-stroke--primary-active': 'var(--bq-grey-300)',
-  'bq-stroke--primary-disabled': 'var(--bq-grey-100)',
-  'bq-stroke--primary-alt': 'var(--bq-grey-50)',
+  'bq-stroke--primary': 'var(--bq-neutral-200)',
+  'bq-stroke--primary-hover': 'var(--bq-neutral-100)',
+  'bq-stroke--primary-active': 'var(--bq-neutral-300)',
+  'bq-stroke--primary-disabled': 'var(--bq-neutral-100)',
+  'bq-stroke--primary-alt': 'var(--bq-neutral-50)',
   /** Secondary */
-  'bq-stroke--secondary': 'var(--bq-grey-600)',
-  'bq-stroke--secondary-hover': 'var(--bq-grey-500)',
-  'bq-stroke--secondary-active': 'var(--bq-grey-700)',
-  'bq-stroke--secondary-disabled': 'var(--bq-grey-300)',
+  'bq-stroke--secondary': 'var(--bq-neutral-600)',
+  'bq-stroke--secondary-hover': 'var(--bq-neutral-500)',
+  'bq-stroke--secondary-active': 'var(--bq-neutral-700)',
+  'bq-stroke--secondary-disabled': 'var(--bq-neutral-300)',
   /** Tertiary */
-  'bq-stroke--tertiary': 'var(--bq-grey-900)',
-  'bq-stroke--tertiary-hover': 'var(--bq-grey-800)',
-  'bq-stroke--tertiary-active': 'var(--bq-grey-1000)',
-  'bq-stroke--tertiary-disabled': 'var(--bq-grey-600)',
+  'bq-stroke--tertiary': 'var(--bq-neutral-900)',
+  'bq-stroke--tertiary-hover': 'var(--bq-neutral-800)',
+  'bq-stroke--tertiary-active': 'var(--bq-neutral-1000)',
+  'bq-stroke--tertiary-disabled': 'var(--bq-neutral-600)',
   /** Inverse */
-  'bq-stroke--inverse': 'var(--bq-neutral-white)',
-  'bq-stroke--inverse-hover': 'var(--bq-grey-50)',
-  'bq-stroke--inverse-active': 'var(--bq-grey-200)',
-  'bq-stroke--inverse-disabled': 'var(--bq-grey-100)',
+  'bq-stroke--inverse': 'var(--bq-white)',
+  'bq-stroke--inverse-hover': 'var(--bq-neutral-50)',
+  'bq-stroke--inverse-active': 'var(--bq-neutral-200)',
+  'bq-stroke--inverse-disabled': 'var(--bq-neutral-100)',
   /** Brand */
-  'bq-stroke--brand': 'var(--bq-iris-600)',
-  'bq-stroke--brand-hover': 'var(--bq-iris-500)',
-  'bq-stroke--brand-active': 'var(--bq-iris-700)',
-  'bq-stroke--brand-disabled': 'var(--bq-iris-400)',
-  'bq-stroke--brand-alt': 'var(--bq-iris-100)',
+  'bq-stroke--brand': 'var(--bq-brand)',
+  'bq-stroke--brand-hover': 'var(--bq-iris-500)', // not found in root
+  'bq-stroke--brand-active': 'var(--bq-iris-700)', // not found in root
+  'bq-stroke--brand-disabled': 'var(--bq-iris-400)', // not found in root
+  'bq-stroke--brand-alt': 'var(--bq-brand-light)',
   /** Success */
-  'bq-stroke--success': 'var(--bq-teal-600)',
-  'bq-stroke--success-hover': 'var(--bq-teal-500)',
-  'bq-stroke--success-active': 'var(--bq-teal-700)',
-  'bq-stroke--success-disabled': 'var(--bq-teal-400)',
+  'bq-stroke--success': 'var(--bq-success)',
+  'bq-stroke--success-hover': 'var(--bq-teal-500)', // not found in root
+  'bq-stroke--success-active': 'var(--bq-teal-700)', // not found in root
+  'bq-stroke--success-disabled': 'var(--bq-teal-400)', // not found in root
   /** Warning */
-  'bq-stroke--warning': 'var(--bq-gold-600)',
-  'bq-stroke--warning-hover': 'var(--bq-gold-500)',
-  'bq-stroke--warning-active': 'var(--bq-gold-700)',
-  'bq-stroke--warning-disabled': 'var(--bq-gold-400)',
+  'bq-stroke--warning': 'var(--bq-warning)',
+  'bq-stroke--warning-hover': 'var(--bq-gold-500)', // not found in root
+  'bq-stroke--warning-active': 'var(--bq-gold-700)', // not found in root
+  'bq-stroke--warning-disabled': 'var(--bq-gold-400)', // not found in root
   /** Danger */
-  'bq-stroke--danger': 'var(--bq-red-600)',
-  'bq-stroke--danger-hover': 'var(--bq-red-500)',
-  'bq-stroke--danger-active': 'var(--bq-red-700)',
-  'bq-stroke--danger-disabled': 'var(--bq-red-400)',
+  'bq-stroke--danger': 'var(--bq-danger)',
+  'bq-stroke--danger-hover': 'var(--bq-red-500)', // not found in root
+  'bq-stroke--danger-active': 'var(--bq-red-700)', // not found in root
+  'bq-stroke--danger-disabled': 'var(--bq-red-400)', // not found in root
   /** Info */
-  'bq-stroke--info': 'var(--bq-blue-600)',
-  'bq-stroke--info-hover': 'var(--bq-blue-500)',
-  'bq-stroke--info-active': 'var(--bq-blue-700)',
-  'bq-stroke--info-disabled': 'var(--bq-blue-400)',
+  'bq-stroke--info': 'var(--bq-brand)',
+  'bq-stroke--info-hover': 'var(--bq-blue-500)', // not found in root
+  'bq-stroke--info-active': 'var(--bq-blue-700)', // not found in root
+  'bq-stroke--info-disabled': 'var(--bq-blue-400)', // not found in root
   /* -------------------------------------------------------------------------- */
   /*                                 Text colors                                */
   /* -------------------------------------------------------------------------- */
   /** Primary */
-  'bq-text--primary': 'var(--bq-grey-800)',
-  'bq-text--primary-hover': 'var(--bq-grey-700)',
-  'bq-text--primary-active': 'var(--bq-grey-900)',
-  'bq-text--primary-disabled': 'var(--bq-grey-500)',
-  'bq-text--primary-alt': 'var(--bq-neutral-white)',
-  'bq-text--primary-alt-disabled': 'var(--bq-grey-200)',
+  'bq-text--primary': 'var(--bq-neutral-800)',
+  'bq-text--primary-hover': 'var(--bq-neutral-700)',
+  'bq-text--primary-active': 'var(--bq-neutral-900)',
+  'bq-text--primary-disabled': 'var(--bq-neutral-500)',
+  'bq-text--primary-alt': 'var(--bq-white)',
+  'bq-text--primary-alt-disabled': 'var(--bq-neutral-200)',
   /** Secondary */
-  'bq-text--secondary': 'var(--bq-grey-600)',
-  'bq-text--secondary-hover': 'var(--bq-grey-500)',
-  'bq-text--secondary-active': 'var(--bq-grey-700)',
-  'bq-text--secondary-disabled': 'var(--bq-grey-400)',
+  'bq-text--secondary': 'var(--bq-neutral-600)',
+  'bq-text--secondary-hover': 'var(--bq-neutral-500)',
+  'bq-text--secondary-active': 'var(--bq-neutral-700)',
+  'bq-text--secondary-disabled': 'var(--bq-neutral-400)',
   /** Inverse */
-  'bq-text--inverse': 'var(--bq-grey-50)',
-  'bq-text--inverse-disabled': 'var(--bq-grey-100)',
+  'bq-text--inverse': 'var(--bq-neutral-50)',
+  'bq-text--inverse-disabled': 'var(--bq-neutral-100)',
   /** Brand */
-  'bq-text--brand': 'var(--bq-iris-600)',
-  'bq-text--brand-hover': 'var(--bq-iris-500)',
-  'bq-text--brand-active': 'var(--bq-iris-700)',
-  'bq-text--brand-disabled': 'var(--bq-iris-400)',
+  'bq-text--brand': 'var(--bq-brand)',
+  'bq-text--brand-hover': 'var(--bq-iris-500)', // not found in root
+  'bq-text--brand-active': 'var(--bq-iris-700)', // not found in root
+  'bq-text--brand-disabled': 'var(--bq-iris-400)', // not found in root
   /** Feedback */
-  'bq-text--info': 'var(--bq-blue-700)',
-  'bq-text--success': 'var(--bq-teal-700)',
-  'bq-text--warning': 'var(--bq-gold-700)',
-  'bq-text--danger': 'var(--bq-red-700)',
+  'bq-text--info': 'var(--bq-brand)',
+  'bq-text--success': 'var(--bq-success)',
+  'bq-text--warning': 'var(--bq-warning)',
+  'bq-text--danger': 'var(--bq-danger)',
   /* -------------------------------------------------------------------------- */
   /*                                  UI colors                                 */
   /* -------------------------------------------------------------------------- */
   /** Primary */
-  'bq-ui--primary': 'var(--bq-neutral-white)',
-  'bq-ui--primary-hover': 'var(--bq-grey-100)',
-  'bq-ui--primary-active': 'var(--bq-grey-300)',
-  'bq-ui--primary-disabled': 'var(--bq-grey-50)',
-  'bq-ui--primary-alt': 'var(--bq-grey-50)',
+  'bq-ui--primary': 'var(--bq-white)',
+  'bq-ui--primary-hover': 'var(--bq-neutral-100)',
+  'bq-ui--primary-active': 'var(--bq-neutral-300)',
+  'bq-ui--primary-disabled': 'var(--bq-neutral-50)',
+  'bq-ui--primary-alt': 'var(--bq-neutral-50)',
   /** Secondary */
-  'bq-ui--secondary': 'var(--bq-grey-200)',
-  'bq-ui--secondary-hover': 'var(--bq-grey-300)',
-  'bq-ui--secondary-active': 'var(--bq-grey-400)',
-  'bq-ui--secondary-disabled': 'var(--bq-grey-100)',
+  'bq-ui--secondary': 'var(--bq-neutral-200)',
+  'bq-ui--secondary-hover': 'var(--bq-neutral-300)',
+  'bq-ui--secondary-active': 'var(--bq-neutral-400)',
+  'bq-ui--secondary-disabled': 'var(--bq-neutral-100)',
   /** Tertiary */
-  'bq-ui--tertiary': 'var(--bq-grey-500)',
-  'bq-ui--tertiary-hover': 'var(--bq-grey-400)',
-  'bq-ui--tertiary-active': 'var(--bq-grey-600)',
-  'bq-ui--tertiary-disabled': 'var(--bq-grey-300)',
+  'bq-ui--tertiary': 'var(--bq-neutral-500)',
+  'bq-ui--tertiary-hover': 'var(--bq-neutral-400)',
+  'bq-ui--tertiary-active': 'var(--bq-neutral-600)',
+  'bq-ui--tertiary-disabled': 'var(--bq-neutral-300)',
   /** Inverse */
-  'bq-ui--inverse': 'var(--bq-grey-900)',
-  'bq-ui--inverse-hover': 'var(--bq-grey-800)',
-  'bq-ui--inverse-active': 'var(--bq-grey-1000)',
-  'bq-ui--inverse-disabled': 'var(--bq-grey-600)',
+  'bq-ui--inverse': 'var(--bq-neutral-900)',
+  'bq-ui--inverse-hover': 'var(--bq-neutral-800)',
+  'bq-ui--inverse-active': 'var(--bq-neutral-1000)',
+  'bq-ui--inverse-disabled': 'var(--bq-neutral-600)',
   /** Brand */
-  'bq-ui--brand': 'var(--bq-iris-600)',
-  'bq-ui--brand-hover': 'var(--bq-iris-500)',
-  'bq-ui--brand-active': 'var(--bq-iris-700)',
-  'bq-ui--brand-disabled': 'var(--bq-iris-400)',
-  'bq-ui--brand-alt': 'var(--bq-iris-100)',
+  'bq-ui--brand': 'var(--bq-brand)',
+  'bq-ui--brand-hover': 'var(--bq-iris-500)', // not found in root
+  'bq-ui--brand-active': 'var(--bq-iris-700)', // not found in root
+  'bq-ui--brand-disabled': 'var(--bq-iris-400)', // not found in root
+  'bq-ui--brand-alt': 'var(--bq-brand-light)',
   /** Success */
-  'bq-ui--success': 'var(--bq-teal-600)',
-  'bq-ui--success-hover': 'var(--bq-teal-500)',
-  'bq-ui--success-active': 'var(--bq-teal-700)',
-  'bq-ui--success-disabled': 'var(--bq-teal-400)',
-  'bq-ui--success-alt': 'var(--bq-teal-100)',
+  'bq-ui--success': 'var(--bq-success)',
+  'bq-ui--success-hover': 'var(--bq-teal-500)', // not found in root
+  'bq-ui--success-active': 'var(--bq-teal-700)', // not found in root
+  'bq-ui--success-disabled': 'var(--bq-teal-400)', // not found in root
+  'bq-ui--success-alt': 'var(--bq-success-light)',
   /** Warning */
-  'bq-ui--warning': 'var(--bq-gold-600)',
-  'bq-ui--warning-hover': 'var(--bq-gold-500)',
-  'bq-ui--warning-active': 'var(--bq-gold-700)',
-  'bq-ui--warning-disabled': 'var(--bq-gold-400)',
-  'bq-ui--warning-alt': 'var(--bq-gold-100)',
+  'bq-ui--warning': 'var(--bq-warning)',
+  'bq-ui--warning-hover': 'var(--bq-gold-500)', // not found in root
+  'bq-ui--warning-active': 'var(--bq-gold-700)', // not found in root
+  'bq-ui--warning-disabled': 'var(--bq-gold-400)', // not found in root
+  'bq-ui--warning-alt': 'var(--bq-warning-light)',
   /** Danger */
-  'bq-ui--danger': 'var(--bq-red-600)',
-  'bq-ui--danger-hover': 'var(--bq-red-500)',
-  'bq-ui--danger-active': 'var(--bq-red-700)',
-  'bq-ui--danger-disabled': 'var(--bq-red-400)',
-  'bq-ui--danger-alt': 'var(--bq-red-100)',
+  'bq-ui--danger': 'var(--bq-danger)',
+  'bq-ui--danger-hover': 'var(--bq-red-500)', // not found in root
+  'bq-ui--danger-active': 'var(--bq-red-700)', // not found in root
+  'bq-ui--danger-disabled': 'var(--bq-red-400)', // not found in root
+  'bq-ui--danger-alt': 'var(--bq-danger-light)',
   /** Info */
-  'bq-ui--info': 'var(--bq-blue-600)',
-  'bq-ui--info-hover': 'var(--bq-blue-500)',
-  'bq-ui--info-active': 'var(--bq-blue-700)',
-  'bq-ui--info-disabled': 'var(--bq-blue-400)',
-  'bq-ui--info-alt': 'var(--bq-blue-100)',
+  'bq-ui--info': 'var(--bq-brand)',
+  'bq-ui--info-hover': 'var(--bq-blue-500)', // not found in root
+  'bq-ui--info-active': 'var(--bq-blue-700)', // not found in root
+  'bq-ui--info-disabled': 'var(--bq-blue-400)', // not found in root
+  'bq-ui--info-alt': 'var(--bq-brand-light)',
 };
