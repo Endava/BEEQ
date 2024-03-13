0b11c805afd62b13e57076df8380c4481d950ee6
Lungan Catalin <catalin.lungan@endava.com>
2024-03-13 12:25:27 +0200

    feat(Drawer): add some basic unit tests

--- packages/beeq/src/components/drawer/__tests__/bq-drawer.e2e.ts
+++ packages/beeq/src/components/drawer/__tests__/bq-drawer.e2e.ts
@@ -18,4 +18,66 @@ describe('bq-drawer', () => {
 
     expect(element.shadowRoot).not.toBeNull();
   });
+
+  it('should render as hidden', async () => {
+    const page = await newE2EPage({
+      html: `<bq-drawer></bq-drawer>`,
+    });
+
+    const element = await page.find('bq-drawer');
+    expect(element).toEqualAttribute('aria-hidden', 'true');
+    expect(element).toHaveClass('is-hidden');
+  });
+
+  it('should render as hidden with `open="false"`', async () => {
+    const page = await newE2EPage({
+      html: `<bq-drawer open="false"></bq-drawer>`,
+    });
+
+    const element = await page.find('bq-drawer');
+    expect(element).toEqualAttribute('aria-hidden', 'true');
+    expect(element).toHaveClass('is-hidden');
+  });
+
+  it('should render as open', async () => {
+    const page = await newE2EPage({
+      html: `<bq-drawer open></bq-drawer>`,
+    });
+
+    const element = await page.find('bq-drawer');
+    expect(element).not.toEqualAttribute('aria-hidden', 'true');
+    expect(element).not.toHaveClass('is-hidden');
+  });
+
+  it('should render as open with `open="true"`', async () => {
+    const page = await newE2EPage({
+      html: `<bq-drawer open="true"></bq-drawer>`,
+    });
+
+    const element = await page.find('bq-drawer');
+    expect(element).not.toEqualAttribute('aria-hidden', 'true');
+    expect(element).not.toHaveClass('is-hidden');
+  });
+
+  it('should display drawer title', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<bq-drawer><slot="title">Drawer Title</bq-drawer>');
+
+    const element = await page.find('bq-drawer');
+
+    expect(element).toEqualText('Drawer Title');
+  });
+
+  it('should render basic body drawer slot', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-drawer>
+          <div slot="body">Slot</div>
+        </bq-drawer>
+      `,
+    });
+
+    const description = await page.find('bq-drawer >>> slot[name="body"]');
+    expect(description).not.toBeNull();
+  });
 });
