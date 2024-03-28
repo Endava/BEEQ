ea2ee9d713a0d780f501e68619e6a759d15c8ff1
Dabiel González Ramos <dabiel.ramos@endava.com>
2024-03-28 20:27:21 +0200

    refactor(beeq): flatten extracted icon assets in the `icon/svg/` folder

--- tools/src/executors/icons/helpers/extract.ts
+++ tools/src/executors/icons/helpers/extract.ts
@@ -1,6 +1,6 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import * as decompress from 'decompress';
-import { copy, remove } from 'fs-extra';
-import { join } from 'node:path';
 
 interface IExtractIcons {
   assetsFolder: string;
@@ -18,13 +18,17 @@ export const extractIcons = async ({
   svgFolder,
 }: IExtractIcons) => {
   try {
-    await decompress(join(downloadPath, fileName), downloadPath);
+    const files = await decompress(path.join(downloadPath, fileName), downloadPath);
     // Remove all existing files under the `/svg/` folder (if there's any) to create a clean copy
-    await remove(extractToPath);
-    // Move the SVG assets to the icon component `/svg/` folder
-    await copy(join(downloadPath, svgFolder, assetsFolder), extractToPath, {
-      overwrite: true,
-    });
+    await fs.remove(extractToPath);
+    // Move the SVG assets to the icon component `/svg/` folder in a flat structure
+    for (const file of files) {
+      if (file.type === 'file' && (file.path as string).includes(path.join(svgFolder, assetsFolder))) {
+        const oldPath = path.join(downloadPath, file.path);
+        const newPath = path.join(extractToPath, path.basename(file.path));
+        await fs.copy(oldPath, newPath, { overwrite: true });
+      }
+    }
     Promise.resolve();
   } catch (error) {
     Promise.reject(error);
