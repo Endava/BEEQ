9b9fe74713c77ed7a6c1ec976c21ed7774810ddf
Lungan Catalin <catalin.lungan@endava.com>
2024-05-08 16:44:54 +0300

    feat(Date Picker): add some e2e tests for component

--- packages/beeq/src/components/date-picker/__tests__/bq-date-picker.e2e.ts
+++ packages/beeq/src/components/date-picker/__tests__/bq-date-picker.e2e.ts
@@ -18,4 +18,48 @@ describe('bq-date-picker', () => {
 
     expect(element.shadowRoot).not.toBeNull();
   });
+
+  it('should render with date picker panel opened', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-date-picker open />
+      `,
+    });
+    const selectPanelElem = await page.find('bq-date-picker >>> .bq-date-picker__dropdown >>> .bq-dropdown__panel');
+
+    expect(selectPanelElem).toHaveAttribute('open');
+  });
+
+  it('should render single type of date picker', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-date-picker open range="false" />
+      `,
+    });
+    const calendarDefaultElement = await page.find('bq-date-picker >>> calendar-date');
+
+    expect(calendarDefaultElement).not.toBeNull();
+  });
+
+  it('should render range type of date picker', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-date-picker open range="true" />
+      `,
+    });
+    const calendarRangeElement = await page.find('bq-date-picker >>> calendar-range');
+
+    expect(calendarRangeElement).not.toBeNull();
+  });
+
+  it('should render multile months for range type of date picker', async () => {
+    const page = await newE2EPage({
+      html: `
+        <bq-date-picker open range months="4" />
+      `,
+    });
+    const calendarMonthElement = await page.findAll('bq-date-picker >>> calendar-month');
+
+    expect(calendarMonthElement.length).toEqual(4);
+  });
 });
