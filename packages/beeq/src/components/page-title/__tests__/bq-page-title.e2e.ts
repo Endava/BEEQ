5ecc72ce0daf75a058264e5bd966a4d56a88922d
Lungan Catalin <catalin.lungan@endava.com>
2024-06-03 17:07:33 +0300

    feat(Page Title): add e2e tests

--- packages/beeq/src/components/page-title/__tests__/bq-page-title.e2e.ts
+++ packages/beeq/src/components/page-title/__tests__/bq-page-title.e2e.ts
@@ -3,7 +3,7 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('bq-page-title', () => {
   it('should render', async () => {
     const page = await newE2EPage({
-      html: '<bq-page-title></bq-page-title>',
+      html: '<bq-page-title>Title</bq-page-title>',
     });
     const element = await page.find('bq-page-title');
 
@@ -12,7 +12,7 @@ describe('bq-page-title', () => {
 
   it('should have shadow root', async () => {
     const page = await newE2EPage({
-      html: '<bq-page-title></bq-page-title>',
+      html: '<bq-page-title>Title</bq-page-title>',
     });
     const element = await page.find('bq-page-title');
 
@@ -21,9 +21,25 @@ describe('bq-page-title', () => {
 
   it('should render title + back icon', async () => {
     const page = await newE2EPage({
-      html: '<bq-page-title showBackIcon="true"></bq-page-title>',
+      html: '<bq-page-title showBackIcon="true">Title</bq-page-title>',
     });
     const backIcon = await page.find('bq-page-title >>> slot[name="back"]');
     expect(backIcon).not.toBeNull();
+  });
+
+  it('should render title + action icons', async () => {
+    const page = await newE2EPage({
+      html: '<bq-page-title showActionIcons="true">Title</bq-page-title>',
+    });
+    const actionIcons = await page.find('bq-page-title >>> slot[name="actions"]');
+    expect(actionIcons).not.toBeNull();
+  });
+
+  it('should render title + sub-title', async () => {
+    const page = await newE2EPage({
+      html: '<bq-page-title showSubTitle="true">Title</bq-page-title>',
+    });
+    const actionIcons = await page.find('bq-page-title >>> slot[name="sub-title"]');
+    expect(actionIcons).not.toBeNull();
   });
 });
