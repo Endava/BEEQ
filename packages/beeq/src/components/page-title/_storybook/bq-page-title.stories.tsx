fbdf7e4534fbc7ec1eaddb95c8ee9b3a4656e6f4
Lungan Catalin <catalin.lungan@endava.com>
2024-06-03 17:06:45 +0300

    feat(Page Title): add stoies for these props in Storybook

--- packages/beeq/src/components/page-title/_storybook/bq-page-title.stories.tsx
+++ packages/beeq/src/components/page-title/_storybook/bq-page-title.stories.tsx
@@ -1,5 +1,5 @@
 import type { Args, Meta, StoryObj } from '@storybook/web-components';
-import { html } from 'lit-html';
+import { html, nothing } from 'lit-html';
 
 import mdx from './bq-page-title.mdx';
 
@@ -13,23 +13,55 @@ const meta: Meta = {
   },
   argTypes: {
     'show-back-icon': { control: 'boolean' },
+    'show-action-icons': { control: 'boolean' },
+    'show-sub-title': { control: 'boolean' },
   },
   args: {
     'show-back-icon': false,
+    'show-action-icons': false,
+    'show-sub-title': false,
   },
 };
 export default meta;
 
 type Story = StoryObj;
 
+const Template = (args: Args) => html`
+  <bq-page-title
+    ?show-back-icon=${args['show-back-icon']}
+    ?show-action-icons=${args['show-action-icons']}
+    ?show-sub-title=${args['show-sub-title']}
+    >Title ${!args['sub-title'] ? html` <div slot="sub-title">Sub-title</div> ` : nothing}
+  </bq-page-title>
+`;
+
 export const Default: Story = {
-  render: (args: Args) => html` <bq-page-title ?show-back-icon=${args['show-back-icon']}> Title </bq-page-title> `,
+  render: Template,
 };
 
 export const TitleBack: Story = {
   name: 'Title + Back',
-  render: (args: Args) => html` <bq-page-title ?show-back-icon=${args['show-back-icon']}> Title </bq-page-title> `,
+  render: Template,
   args: {
     'show-back-icon': true,
+  },
+};
+
+export const TitleBackSubtitle: Story = {
+  name: 'Title + Back + Subtitle',
+  render: Template,
+  args: {
+    'show-back-icon': true,
+    'show-sub-title': true,
+  },
+};
+
+export const TitleBackActions: Story = {
+  name: 'Title + Back + Subtitle + Actions',
+  render: Template,
+  args: {
+    'show-back-icon': true,
+    'show-action-icons': true,
+    'show-sub-title': true,
   },
 };
