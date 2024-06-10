66e4f0324b89b04889a0f7d602deaeeaef83154b
Dabiel González Ramos <dgonzalezr@users.noreply.github.com>
2024-06-10 22:50:29 +0300

    chore(Tailwind CSS): add support for missing CSS logical properties (#1095)

---
+++ packages/beeq-tailwindcss/src/plugins/color-mix.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+
+import { blendColor } from '../helpers';
+
+// NOTE: https://github.com/tailwindlabs/tailwindcss/discussions/6925#discussioncomment-1919382
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
+
+export const ColorMix: Partial<Config> = plugin(function ({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      // Background `hover` state blend color
+      'bg-hover': (value) => blendColor({ color: value, base: 'var(--bq-hover)' }),
+      // Background `active` state blend color
+      'bg-active': (value) => blendColor({ color: value, base: 'var(--bq-active)' }),
+      // Border `hover` state blend color
+      'border-hover': (value) => blendColor({ color: value, base: 'var(--bq-hover)', property: 'border-color' }),
+      // Border `active` state blend color
+      'border-active': (value) => blendColor({ color: value, base: 'var(--bq-active)', property: 'border-color' }),
+      // Text `hover` state blend color
+      'text-hover': (value) => blendColor({ color: value, base: 'var(--bq-hover)', property: 'color' }),
+      // Text `active` state blend color
+      'text-active': (value) => blendColor({ color: value, base: 'var(--bq-active)', property: 'color' }),
+    },
+    { values: flattenColorPalette(theme('colors')) },
+  );
+});
