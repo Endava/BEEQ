ad80a3edebbca7cb529de888e30c37142b9ef835
Dabiel González Ramos <dabiel.ramos@endava.com>
2024-10-09 12:41:39 +0300

    chore(BEEQ React): move ESLint configuration to flat config

---
+++ packages/beeq-react/eslint.config.js
@@ -0,0 +1,41 @@
+const nxESLint = require('@nx/eslint-plugin');
+const jsoncParser = require('jsonc-eslint-parser');
+const tsESLint = require('typescript-eslint');
+
+/** @type { import("eslint").Linter.Config[] } */
+module.exports = [
+  ...tsESLint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    files: ['**/package.json', '**/project.json'],
+    languageOptions: {
+      parser: jsoncParser,
+    },
+    plugins: {
+      '@nx': nxESLint,
+    },
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredDependencies: ['@beeq/core', 'react', 'react-dom'],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+];
