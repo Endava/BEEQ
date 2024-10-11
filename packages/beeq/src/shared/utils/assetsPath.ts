ff656816ee29425f31aa974e5261b2769e436746
Dabiel González Ramos <dabiel.ramos@endava.com>
2024-10-11 14:26:24 +0300

    refactor(Assets path): avoid the use of `document.query` out of a method/function

--- packages/beeq/src/shared/utils/assetsPath.ts
+++ packages/beeq/src/shared/utils/assetsPath.ts
@@ -3,64 +3,71 @@
  * https://github.com/shoelace-style/shoelace/blob/next/src/utilities/base-path.ts
  */
 
-let bqSVGBasePath = '';
-
+let basePath: string | undefined;
 const DATA_BEEQ_ATTRIBUTE = 'data-beeq';
 const DEFAULT_SVG_PATH = 'svg';
-const scripts: HTMLScriptElement[] =
-  document && typeof document !== 'undefined'
-    ? ([...document.getElementsByTagName('script')] as HTMLScriptElement[])
-    : [];
 
 /**
- * Sets the `bqSVGBasePath` in the global window object,
- * which is used to determine the SVG path in the BQIcon component.
- * This method is the only way to update the base path.
- *
- * @param path - The new base path to set.
+ * Sets the base path for the assets.
+ * @param {string} path - The base path to set.
  */
 export const setBasePath = (path: string): void => {
-  bqSVGBasePath = path;
+  basePath = path;
 };
 
 /**
- * Retrieves the `bqSVGBasePath`, optionally appending a subpath.
- *
- * @param subpath - The subpath to append to the base path.
- * @returns The full base path including the subpath.
+ * Gets the base path for the assets.
+ * @param {string} subpath - An optional subpath to append to the base path.
+ * @returns {string} The base path of the assets.
  */
-export const getBasePath = (subpath = ''): string => {
-  if (!bqSVGBasePath) {
-    initializeBasePath();
+export const getBasePath = (subpath: string = ''): string => {
+  if (!basePath) {
+    const configScript = findConfigScript();
+    const fallbackScript = configScript ? null : findFallbackScript();
+
+    const script = configScript || fallbackScript;
+    if (script) {
+      const path = configScript ? script.getAttribute(DATA_BEEQ_ATTRIBUTE) : getScriptPath(script);
+      setBasePath(`${path}/${DEFAULT_SVG_PATH}`);
+    } else {
+      // Fallback: use an environment variable (if set) or the default path
+      setBasePath(process.env.ASSETS_BASE_PATH || `./${DEFAULT_SVG_PATH}`);
+    }
   }
 
+  // Return the base path without a trailing slash. If one exists, append the subpath separated by a slash.
   const formattedSubpath = subpath ? `/${subpath.replace(/^\//, '')}` : '';
-  return bqSVGBasePath.replace(/\/$/, '') + formattedSubpath;
+  return basePath.replace(/\/$/, '') + formattedSubpath;
 };
 
 /**
- * Initializes the `bqSVGBasePath` for the BEEQ assets by finding the appropriate script and setting the base path accordingly.
- * 1. If a script with the attribute `data-beeq` exists, the base path will be set to the value of the `src` attribute.
- * 2. If a script with the name `beeq.js` or `beeq.esm.js` exists, the base path will be set to the value of the `src` attribute.
- * 3. If no scripts are found, the base path will be set to the default value `./svg`.
+ * Finds the configuration script element.
+ * @returns The configuration script element or null if not found.
  */
-const initializeBasePath = (): void => {
-  const configScript = findConfigScript();
-  const fallbackScript = configScript ? null : findFallbackScript();
+const findConfigScript = (): HTMLScriptElement | null => {
+  if (typeof window === 'undefined') return null;
 
-  const script = configScript || fallbackScript;
-  if (script) {
-    const path = configScript ? script.getAttribute(DATA_BEEQ_ATTRIBUTE) : getScriptPath(script);
-    setBasePath(`${path}/${DEFAULT_SVG_PATH}`);
-  } else {
-    setBasePath(`./${DEFAULT_SVG_PATH}`);
-  }
+  return document.querySelector(`script[${DATA_BEEQ_ATTRIBUTE}]`);
 };
 
-const findConfigScript = (): HTMLScriptElement => scripts?.find((script) => script.hasAttribute(DATA_BEEQ_ATTRIBUTE));
+/**
+ * Finds the fallback script element.
+ * @returns The fallback script element or null if not found.
+ */
+const findFallbackScript = (): HTMLScriptElement | null => {
+  if (typeof window === 'undefined') return null;
 
-const findFallbackScript = (): HTMLScriptElement =>
-  scripts?.find((script) => /beeq(\.esm)?\.js($|\?)/.test(script.src));
+  return document.querySelector(`script[src*="beeq"]`);
+};
 
-const getScriptPath = (script: HTMLScriptElement): string =>
-  script?.getAttribute('src').split('/').slice(0, -1).join('/');
+/**
+ * Gets the path of a script element.
+ * @param {HTMLScriptElement} script - The script element.
+ * @returns The path of the script element.
+ */
+const getScriptPath = (script: HTMLScriptElement): string => {
+  if (typeof window === 'undefined') return '';
+
+  const src = script.getAttribute('src');
+  return src ? src.substring(0, src.lastIndexOf('/')) : '';
+};
