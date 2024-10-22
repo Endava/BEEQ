a0b0f392b0bbaf6b5013b38fd0025a37745e60c1
Dabiel González Ramos <dabiel.ramos@endava.com>
2024-10-22 14:27:24 +0300

    refactor(Switch): use Tailwind colors from the corresponding core plugin

--- packages/beeq/src/components/switch/bq-switch.tsx
+++ packages/beeq/src/components/switch/bq-switch.tsx
@@ -230,8 +230,6 @@ export class BqSwitch {
                 part="icon-on"
               />
             )}
-            {/* Dot */}
-            <div class="bq-switch--control__dot" part="dot" />
             {this.innerLabel === 'icon' && (
               <bq-icon
                 class="bq-switch--control__icon off"
@@ -242,6 +240,8 @@ export class BqSwitch {
                 part="icon-off"
               />
             )}
+            {/* Dot */}
+            <div class="bq-switch--control__dot" part="dot" />
           </div>
           {/* Label */}
           <span
