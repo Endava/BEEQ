cc1682220ea93fd4133b7d6f18e11b340cb389ed
Dabiel González Ramos <dabiel.ramos@endava.com>
2024-11-27 20:24:01 +0200

    docs(Toast): add JSDoc annotations for the Custom Elements Manifest

--- packages/beeq/src/components.d.ts
+++ packages/beeq/src/components.d.ts
@@ -2730,6 +2730,44 @@ export namespace Components {
          */
         "wrap": TTextareaWrap;
     }
+    /**
+     * Toasts are time-based components used to display short messages.
+     * Commonly used for errors, confirmations, or progress updates.
+     * @example How to use it
+     * ```html
+     * <bq-toast type="info">
+     * This is a message
+     * </bq-toast>
+     * ```
+     * @documentation https://www.beeq.design/3d466e231/p/83da51-toast
+     * @status stable
+     * @attr {"s" | "none" | "xs2" | "xs" | "m" | "l" | "full"} border - The corder radius of the toast component
+     * @attr {"success" | "error" | "loading" | "alert" | "info"} type - Type of toast
+     * @attr {"top-left" | "top-center" | "top-right" | "bottom-left" | "bottom-center" | "bottom-right"} placement - Placement of toast
+     * @attr {boolean} hide-icon - If true will hide toast icon
+     * @attr {boolean} open - If true, the toast will be shown
+     * @attr {number} time - The length of time, in milliseconds, after which the toast will close itself
+     * @method show - Method to be called to show the toast component
+     * @method hide - Method to be called to hide the toast component
+     * @method toast - This method can be used to display toasts in a fixed-position element that allows for stacking multiple toasts vertically
+     * @event bqHide - Callback handler to be called when the notification is hidden
+     * @event bqShow - Callback handler to be called when the notification is shown
+     * @cssprop --bq-toast--background - Toast background color
+     * @cssprop --bq-toast--box-shadow - Toast box shadow
+     * @cssprop --bq-toast--padding-y - Toast vertical padding
+     * @cssprop --bq-toast--padding-x - Toast horizontal padding
+     * @cssprop --bq-toast--gap - Toast distance between icon and text
+     * @cssprop --bq-toast--border-radius - Toast border radius
+     * @cssprop --bq-toast--border-color - Toast border color
+     * @cssprop --bq-toast--border-style - Toast border style
+     * @cssprop --bq-toast--border-width - Toast border width
+     * @cssprop --bq-toast--icon-color-info - Toast icon color when type is 'info'
+     * @cssprop --bq-toast--icon-color-success - Toast icon color when type is 'success'
+     * @cssprop --bq-toast--icon-color-alert - Toast icon color when type is 'alert'
+     * @cssprop --bq-toast--icon-color-error - Toast icon color when type is 'error'
+     * @cssprop --bq-toast--icon-color-loading - Toast icon color when type is 'loading'
+     * @cssprop --bq-toast--icon-color-custom - Toast icon color when type is 'custom'
+     */
     interface BqToast {
         /**
           * The corder radius of the toast component
@@ -4839,6 +4877,44 @@ declare global {
         "bqHide": HTMLBqToastElement;
         "bqShow": HTMLBqToastElement;
     }
+    /**
+     * Toasts are time-based components used to display short messages.
+     * Commonly used for errors, confirmations, or progress updates.
+     * @example How to use it
+     * ```html
+     * <bq-toast type="info">
+     * This is a message
+     * </bq-toast>
+     * ```
+     * @documentation https://www.beeq.design/3d466e231/p/83da51-toast
+     * @status stable
+     * @attr {"s" | "none" | "xs2" | "xs" | "m" | "l" | "full"} border - The corder radius of the toast component
+     * @attr {"success" | "error" | "loading" | "alert" | "info"} type - Type of toast
+     * @attr {"top-left" | "top-center" | "top-right" | "bottom-left" | "bottom-center" | "bottom-right"} placement - Placement of toast
+     * @attr {boolean} hide-icon - If true will hide toast icon
+     * @attr {boolean} open - If true, the toast will be shown
+     * @attr {number} time - The length of time, in milliseconds, after which the toast will close itself
+     * @method show - Method to be called to show the toast component
+     * @method hide - Method to be called to hide the toast component
+     * @method toast - This method can be used to display toasts in a fixed-position element that allows for stacking multiple toasts vertically
+     * @event bqHide - Callback handler to be called when the notification is hidden
+     * @event bqShow - Callback handler to be called when the notification is shown
+     * @cssprop --bq-toast--background - Toast background color
+     * @cssprop --bq-toast--box-shadow - Toast box shadow
+     * @cssprop --bq-toast--padding-y - Toast vertical padding
+     * @cssprop --bq-toast--padding-x - Toast horizontal padding
+     * @cssprop --bq-toast--gap - Toast distance between icon and text
+     * @cssprop --bq-toast--border-radius - Toast border radius
+     * @cssprop --bq-toast--border-color - Toast border color
+     * @cssprop --bq-toast--border-style - Toast border style
+     * @cssprop --bq-toast--border-width - Toast border width
+     * @cssprop --bq-toast--icon-color-info - Toast icon color when type is 'info'
+     * @cssprop --bq-toast--icon-color-success - Toast icon color when type is 'success'
+     * @cssprop --bq-toast--icon-color-alert - Toast icon color when type is 'alert'
+     * @cssprop --bq-toast--icon-color-error - Toast icon color when type is 'error'
+     * @cssprop --bq-toast--icon-color-loading - Toast icon color when type is 'loading'
+     * @cssprop --bq-toast--icon-color-custom - Toast icon color when type is 'custom'
+     */
     interface HTMLBqToastElement extends Components.BqToast, HTMLStencilElement {
         addEventListener<K extends keyof HTMLBqToastElementEventMap>(type: K, listener: (this: HTMLBqToastElement, ev: BqToastCustomEvent<HTMLBqToastElementEventMap[K]>) => any, options?: boolean | AddEventListenerOptions): void;
         addEventListener<K extends keyof DocumentEventMap>(type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
@@ -7798,6 +7874,44 @@ declare namespace LocalJSX {
          */
         "wrap"?: TTextareaWrap;
     }
+    /**
+     * Toasts are time-based components used to display short messages.
+     * Commonly used for errors, confirmations, or progress updates.
+     * @example How to use it
+     * ```html
+     * <bq-toast type="info">
+     * This is a message
+     * </bq-toast>
+     * ```
+     * @documentation https://www.beeq.design/3d466e231/p/83da51-toast
+     * @status stable
+     * @attr {"s" | "none" | "xs2" | "xs" | "m" | "l" | "full"} border - The corder radius of the toast component
+     * @attr {"success" | "error" | "loading" | "alert" | "info"} type - Type of toast
+     * @attr {"top-left" | "top-center" | "top-right" | "bottom-left" | "bottom-center" | "bottom-right"} placement - Placement of toast
+     * @attr {boolean} hide-icon - If true will hide toast icon
+     * @attr {boolean} open - If true, the toast will be shown
+     * @attr {number} time - The length of time, in milliseconds, after which the toast will close itself
+     * @method show - Method to be called to show the toast component
+     * @method hide - Method to be called to hide the toast component
+     * @method toast - This method can be used to display toasts in a fixed-position element that allows for stacking multiple toasts vertically
+     * @event bqHide - Callback handler to be called when the notification is hidden
+     * @event bqShow - Callback handler to be called when the notification is shown
+     * @cssprop --bq-toast--background - Toast background color
+     * @cssprop --bq-toast--box-shadow - Toast box shadow
+     * @cssprop --bq-toast--padding-y - Toast vertical padding
+     * @cssprop --bq-toast--padding-x - Toast horizontal padding
+     * @cssprop --bq-toast--gap - Toast distance between icon and text
+     * @cssprop --bq-toast--border-radius - Toast border radius
+     * @cssprop --bq-toast--border-color - Toast border color
+     * @cssprop --bq-toast--border-style - Toast border style
+     * @cssprop --bq-toast--border-width - Toast border width
+     * @cssprop --bq-toast--icon-color-info - Toast icon color when type is 'info'
+     * @cssprop --bq-toast--icon-color-success - Toast icon color when type is 'success'
+     * @cssprop --bq-toast--icon-color-alert - Toast icon color when type is 'alert'
+     * @cssprop --bq-toast--icon-color-error - Toast icon color when type is 'error'
+     * @cssprop --bq-toast--icon-color-loading - Toast icon color when type is 'loading'
+     * @cssprop --bq-toast--icon-color-custom - Toast icon color when type is 'custom'
+     */
     interface BqToast {
         /**
           * The corder radius of the toast component
@@ -9285,6 +9399,44 @@ declare module "@stencil/core" {
              * @prop --bq-textarea--text-placeholder-color - Textarea placeholder text color
              */
             "bq-textarea": LocalJSX.BqTextarea & JSXBase.HTMLAttributes<HTMLBqTextareaElement>;
+            /**
+             * Toasts are time-based components used to display short messages.
+             * Commonly used for errors, confirmations, or progress updates.
+             * @example How to use it
+             * ```html
+             * <bq-toast type="info">
+             * This is a message
+             * </bq-toast>
+             * ```
+             * @documentation https://www.beeq.design/3d466e231/p/83da51-toast
+             * @status stable
+             * @attr {"s" | "none" | "xs2" | "xs" | "m" | "l" | "full"} border - The corder radius of the toast component
+             * @attr {"success" | "error" | "loading" | "alert" | "info"} type - Type of toast
+             * @attr {"top-left" | "top-center" | "top-right" | "bottom-left" | "bottom-center" | "bottom-right"} placement - Placement of toast
+             * @attr {boolean} hide-icon - If true will hide toast icon
+             * @attr {boolean} open - If true, the toast will be shown
+             * @attr {number} time - The length of time, in milliseconds, after which the toast will close itself
+             * @method show - Method to be called to show the toast component
+             * @method hide - Method to be called to hide the toast component
+             * @method toast - This method can be used to display toasts in a fixed-position element that allows for stacking multiple toasts vertically
+             * @event bqHide - Callback handler to be called when the notification is hidden
+             * @event bqShow - Callback handler to be called when the notification is shown
+             * @cssprop --bq-toast--background - Toast background color
+             * @cssprop --bq-toast--box-shadow - Toast box shadow
+             * @cssprop --bq-toast--padding-y - Toast vertical padding
+             * @cssprop --bq-toast--padding-x - Toast horizontal padding
+             * @cssprop --bq-toast--gap - Toast distance between icon and text
+             * @cssprop --bq-toast--border-radius - Toast border radius
+             * @cssprop --bq-toast--border-color - Toast border color
+             * @cssprop --bq-toast--border-style - Toast border style
+             * @cssprop --bq-toast--border-width - Toast border width
+             * @cssprop --bq-toast--icon-color-info - Toast icon color when type is 'info'
+             * @cssprop --bq-toast--icon-color-success - Toast icon color when type is 'success'
+             * @cssprop --bq-toast--icon-color-alert - Toast icon color when type is 'alert'
+             * @cssprop --bq-toast--icon-color-error - Toast icon color when type is 'error'
+             * @cssprop --bq-toast--icon-color-loading - Toast icon color when type is 'loading'
+             * @cssprop --bq-toast--icon-color-custom - Toast icon color when type is 'custom'
+             */
             "bq-toast": LocalJSX.BqToast & JSXBase.HTMLAttributes<HTMLBqToastElement>;
             "bq-tooltip": LocalJSX.BqTooltip & JSXBase.HTMLAttributes<HTMLBqTooltipElement>;
         }
