0727447c9e5ebd0eb93b496f9e14298811fd65bc
Dabiel González Ramos <dabiel.ramos@endava.com>
2025-02-10 20:37:05 +0200

    refactor: load cally calendar library from CDN instead of bundling

--- packages/beeq/src/components/date-picker/bq-date-picker.types.ts
+++ packages/beeq/src/components/date-picker/bq-date-picker.types.ts
@@ -2,3 +2,20 @@ export type DaysOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
 export const DATE_PICKER_TYPE = ['single', 'multi', 'range'] as const;
 export type TDatePickerType = (typeof DATE_PICKER_TYPE)[number];
+
+export type TCalendarDate = {
+  focusedDate: string;
+  value: string;
+  min?: string;
+  max?: string;
+  months?: number;
+  tentative?: string;
+  pageBy?: 'single' | 'months';
+  firstDayOfWeek?: number;
+  showOutsideDays?: boolean;
+  isDateDisallowed?: (date: Date) => boolean;
+  locale?: string;
+  onChange?: (ev: Event) => void;
+  onRangestart?: (ev: CustomEvent) => void;
+  onRangeend?: (ev: CustomEvent) => void;
+};
