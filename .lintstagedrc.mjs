0d4b37d7dabef0ef00ba15e3c9d85ad213482848
Dabiel González Ramos <dabiel.ramos@endava.com>
2025-06-06 21:04:58 +0300

    chore(lint): disable TypeScript project-aware linting for Storybook config files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,11 @@
+import { relative } from 'path';
+
 export default {
   // Run Prettier
   '{apps,packages,tools}/**/*.{js,json,ts,tsx,scss}': (files) => `pnpm exec nx format:write --files=${files.join(',')}`,
-  // Run ESLint
-  'packages/beeq/src/**/*.*': ["pnpm exec nx affected -t lint --exclude='*,!tag:publishable' -- --fix=true"],
+  // Run linter for beeq package only
+  'packages/beeq/src/**/*.*': (files) => {
+    const relativeFiles = files.map((file) => relative(process.cwd(), file));
+    return `pnpm exec nx affected -t lint --exclude='*,!tag:core' --fix --files=${relativeFiles.join(',')}`;
+  },
 };
