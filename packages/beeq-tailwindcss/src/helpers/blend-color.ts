309b5e1c14548a99ca88231d7faa6595ef1b3cbe
Dabiel González Ramos <dabiel.ramos@endava.com>
2025-10-09 21:23:06 +1000

    chore(packages/beeq-tailwindcss): migrate ESLint to Biomejs

--- packages/beeq-tailwindcss/src/helpers/blend-color.ts
+++ packages/beeq-tailwindcss/src/helpers/blend-color.ts
@@ -1,9 +1,10 @@
-type blendOptions = {
+type BlendOptions = {
   color: string;
   base: string;
   percentage?: number;
   property?: string;
 };
+
 /**
  * Blend color with base color and percentage
  *
@@ -13,6 +14,6 @@ type blendOptions = {
  * @param {number} opt.percentage - The percentage to blend
  * @param {string} [opt.property ='background-color'] - The CSS property to blend (e.g: 'background-color', 'color')
  */
-export const blendColor = ({ color, base, percentage = 20, property = 'background-color' }: blendOptions) => ({
+export const blendColor = ({ color, base, percentage = 20, property = 'background-color' }: BlendOptions) => ({
   [property]: `color-mix(in srgb, ${color}, ${base} ${percentage}%)`,
 });
