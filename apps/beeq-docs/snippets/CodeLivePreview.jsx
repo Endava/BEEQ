55110b77f0ffdce4856efa559cc831f4970ce4bf
Dabiel Gonzalez-Ramos <dabiel.ramos@endava.com>
2026-04-23 10:49:05 +1000

    docs(CodeLivePreview): add biome comment exception for use of dangerouslySetInnerHTML

--- apps/beeq-docs/snippets/CodeLivePreview.jsx
+++ apps/beeq-docs/snippets/CodeLivePreview.jsx
@@ -69,6 +69,7 @@ export const CodeLivePreview = ({ code, children, height }) => {
         className="preview"
         ref={previewRef}
         // biome-ignore lint/style/useNamingConvention: `__html` is required for dangerouslySetInnerHTML
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: This is a controlled use of dangerouslySetInnerHTML for rendering code snippets in documentation.
         dangerouslySetInnerHTML={{ __html: code }}
         style={{ minHeight: height }}
       />
